913b2179c6b4253b92f663ac0e7edc23248e80fa
Mayrton <mayrtondias@hotmail.com>
2014-01-17 09:18:40 -0300

    Criando valida

---
+++ valida.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>GuaraGas</title>
+    </head>
+   
+    <body>
+        <?php
+                 
+            
+        require 'banco.php';
+           
+            session_start();
+           
+            $banco=new banco(); 
+        ?>
+    </body>
+</html>

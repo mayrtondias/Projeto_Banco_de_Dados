b5faea46820ba3c839f8ceb8bc39ec1ed76ffd94
Mayrton <mayrtondias@hotmail.com>
2014-01-16 23:07:48 -0300

    Iniciando projeto

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            echo "opa";
+        ?>
+        <!--
+            
+            $conn = pg_connect("host=localhost dbname=postgres user=postgres  password=12345678 port=5432") or die ('Erro  ao conectar com o servidor');
+            $reg_query = "INSERT INTO teste (nome, senha) VALUES ('Vinicius', 10);";
+            $res = pg_query($conn, $reg_query) or die("Nao foi possivel executar a query: $reg_query\n");
+            ?>-->
+        <h1>mentira</h1>
+    </body>
+</html>

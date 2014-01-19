0b101c1c350ba07e49438c041cf9f5485a74aadf
Mayrton <mayrtondias@hotmail.com>
2014-01-18 22:44:20 -0300

    criando menu funcionario 1.0

--- menuFuncionario.php
+++ menuFuncionario.php
@@ -1,8 +1,21 @@
 <?php
+    session_start();
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+    if( ( isset($_SESSION['nome']) == FALSE)||( isset($_SESSION['email']) == FALSE) ){
+        header('location: desconectado.php');
+    }
+?>
+    <div  style="border-radius:1em; width:20%; font-size:25px; background:#3399FF;float: left">  
+        <center>
+            <a href=".php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Home         </b></a><br><hr>
+            
+            <a href="sair.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Sair         </b></a><br>
+        </center>
+    </div>
+    <!-- logout!-->
+    <div  style="border-radius:1em; width:25%; font-size:12px;background:#00BFFF; float: right; border-bottom: 5px">
+        <center>
+            <a href="sair.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>Olá <?php echo $_SESSION['nome'];?>, <i style="">Logout</i></b></a><br>
+        </center>
+    </div>
+    <br>

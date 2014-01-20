fdcb231bd93c55ccc18024d4b25e4c6bc74e1f7f
Mayrton <mayrtondias@hotmail.com>
2014-01-20 08:47:58 -0300

    update menuAdministrador

--- menuAdministrador.php
+++ menuAdministrador.php
@@ -1,14 +1,17 @@
 <?php
     session_start();
 
-    if( ( isset($_SESSION['nome']) == FALSE)||( isset($_SESSION['email']) == FALSE) ){
+    if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
         header('location: desconectado.php');
     }
 ?>
     <div  style="border-radius:1em; width:20%; font-size:25px; background:#3399FF;float: left">  
         <center>
-            <a href=".php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Home         </b></a><br><hr>
-            
+            <a href="adminHome.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Home         </b></a><br><hr>
+            <a href="adminCadAdmin.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cadastrar Administrador         </b></a><br><hr>
+            <a href="adminCadFunc.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cadastrar Funcionario         </b></a><br><hr>
+            <a href="adminCadFunc.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Controle de Vendas        </b></a><br><hr>
+            <a href="adminCadFunc.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Controle Produtos        </b></a><br><hr>
             <a href="sair.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Sair         </b></a><br>
         </center>
     </div>

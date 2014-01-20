45a692ba77c3eb1827b8830a3f15474b13f05d87
Mayrton <mayrtondias@hotmail.com>
2014-01-19 22:13:08 -0300

    update indexErro

--- indexErro.php
+++ indexErro.php
@@ -1,8 +1,29 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+     
+    <body>
+        <center>
+            <div>
+                <form action="valida.php" method="post">
+                    Login:<br>
+                    <input type="text" name="login"><br>
+                    senha:<br>
+                    <input type="password" name="senha"><br>
+                    Atenção: Não foi possivel validar seu acesso! 
+                    Caso voçe seja um usuario cadastrado verifique seu Login e Senha
+                    Caso contrario, entre contador com o Administrador.
+                    <input type="submit" value="Entrar">
+                </form>
+            </div>
+        </center>
+    </body>
+</html>

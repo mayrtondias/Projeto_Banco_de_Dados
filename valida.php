2642c2a48b7ce41bfa993c60a1e889708462a617
Mayrton <mayrtondias@hotmail.com>
2014-01-19 22:12:41 -0300

    update valida

--- valida.php
+++ valida.php
@@ -6,14 +6,57 @@
     <body>
         <?php
         
-        require 'banco.php';
-           
+            require 'banco.php';
+            $banco=new banco();
             session_start();
-           
-            $banco=new banco(); 
             
-            $banco->inserir();
-            echo"sim";
+            $login = $_POST['login'];      
+            $senha = $_POST['senha'];
+            
+            $_SESSION['login']==="";
+            $_SESSION['senha']==="";
+            
+            $tabela="administrador";
+            $pesquisa="*";
+            
+            $resultado=$banco->pesquisar($pesquisa, $tabela);
+
+            if($resultado==NULL){
+                echo "Problema com a pesquisa! ".pg_errormessage();
+            } else{
+                while($registro = pg_fetch_array($resultado)){
+                    
+                    if(($registro['login']===$login)&&($registro['senha']===$senha)){
+                        $_SESSION['login']=$registro['login'];
+                        $_SESSION['senha']=$registro['senha'];
+                        
+                        header('location: adminHome.php');
+                    }
+                }
+            }
+            
+            $tabela="funcionario";            
+            $resultado=$banco->pesquisar($pesquisa, $tabela);
+
+            if($resultado==NULL){
+                echo "Problema com a pesquisa! ".pg_errormessage();
+            } else{
+                while($registro = pg_fetch_array($resultado)){
+                    
+                    if(($registro['login']===$login)&&($registro['senha']===$senha)){
+                        $_SESSION['login']=$registro['login'];
+                        $_SESSION['senha']=$registro['senha'];
+                        header('location: funcHome.php');
+                    }
+                }
+            }
+            echo " ".$_SESSION['login']." - ".$_SESSION['senha']." <br>Jesus<br>";
+            if( ($_SESSION['login']==="")&&($_SESSION['senha']==="")){
+                $_SESSION['login']="erro";
+                $_SESSION['senha']="erro";
+                header('location: indexErro.php');
+            }
+            echo "<br>fim<br>";
         ?>
     </body>
 </html>

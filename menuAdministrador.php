7b555514da397f3866041ca0c89c9c346af7b0f6
Mayrton <mayrtondias@hotmail.com>
2014-01-20 22:57:25 -0300

    update menuAdministrador

--- menuAdministrador.php
+++ menuAdministrador.php
@@ -9,16 +9,16 @@
         <center>
             <a href="adminHome.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Home         </b></a><br><hr>
             <a href="adminCadAdmin.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cadastrar Administrador         </b></a><br><hr>
-            <a href="adminCadFunc.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cadastrar Funcionario         </b></a><br><hr>
-            <a href="adminCadFunc.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Controle de Vendas        </b></a><br><hr>
-            <a href="adminCadFunc.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Controle Produtos        </b></a><br><hr>
+            <a href="adminCadFunc.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Gerenciar Funcionario         </b></a><br><hr>
+            <a href="adminEstoque.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Estoque        </b></a><br><hr>
+            <a href="adminFinancas.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Finanças        </b></a><br><hr>
             <a href="sair.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Sair         </b></a><br>
         </center>
     </div>
     <!-- logout!-->
     <div  style="border-radius:1em; width:25%; font-size:12px;background:#00BFFF; float: right; border-bottom: 5px">
         <center>
-            <a href="sair.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>Olá <?php echo $_SESSION['nome'];?>, <i style="">Logout</i></b></a><br>
+            <a href="sair.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>Olá <?php echo $_SESSION['login'];?>, <i style="">Logout</i></b></a><br>
         </center>
     </div>
     <br>

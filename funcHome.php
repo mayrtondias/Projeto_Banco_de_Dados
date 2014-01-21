4c9c76d5c47b38028fdea4eaad54079e566d7876
Mayrton <mayrtondias@hotmail.com>
2014-01-20 22:57:00 -0300

    update funcHome

--- funcHome.php
+++ funcHome.php
@@ -1,8 +1,18 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>GuaraGas</title>
+    </head>
+     
+    <body>
+        <?php
+            require 'menuFuncionario.php';
+        ?>
+    </body>
+</html>

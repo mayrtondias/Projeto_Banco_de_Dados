2fd93575714347fa06847c1d4ba3e08517b23811
Mayrton <mayrtondias@hotmail.com>
2014-01-24 22:20:53 -0300

    update adminHomeFunc

--- adminHomeFunc.php
+++ adminHomeFunc.php
@@ -1,8 +1,25 @@
-<?php
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>GuaraGas</title>
+    </head>
+     
+    <body>
+        <?php
+            require 'menuAdministrador.php';
+        ?>
+        
+        <center>
+            <div>
+                sim
+            </div>
+        </center>
+    </body>
+</html>

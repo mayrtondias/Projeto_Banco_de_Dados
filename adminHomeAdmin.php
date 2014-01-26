8da09b4b855ad6cec31b982e56505ba9c2211470
Mayrton <mayrtondias@hotmail.com>
2014-01-25 22:03:58 -0300

    update adminHomeAdmin

--- adminHomeAdmin.php
+++ adminHomeAdmin.php
@@ -16,15 +16,16 @@ and open the template in the editor.
             require 'menuAdministrador.php';
         ?>
         
-        
-        <div  style="border-radius:1em; width:20%; font-size:25px; background:#3399FF;float: left">  
-            <center>
-                <a href="adminHomeAdminCad.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cadastrar Administrador         </b></a><br><hr>
-                <a href="adminHomeAdminList.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Listar Administradores         </b></a><br><hr>
-                <a href="adminHomeFuncDelete.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Excluir Administrador         </b></a><br><hr>
-                <a href="adminHomeEstoqueAtualizar.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Atualizar Conta        </b></a><br><hr>
-                <a href="sair.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Sair         </b></a><br>
-            </center>
-        </div>
+        <center>
+            <div  style="border-radius:1em; width:20%; font-size:25px; background:#3300FF;float: left;border: 30px">  
+                <center>
+                    <a href="adminHomeAdminCad.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cadastrar Administrador         </b></a><br><hr>
+                    <a href="adminHomeAdminList.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Listar Administradores         </b></a><br><hr>
+                    <a href="adminHomeFuncDelete.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Excluir Administrador         </b></a><br><hr>
+                    <a href="adminHomeEstoqueAtualizar.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Atualizar Conta        </b></a><br><hr>
+                    <a href="sair.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Sair         </b></a><br>
+                </center>
+            </div>
+        </center>
     </body>
 </html>

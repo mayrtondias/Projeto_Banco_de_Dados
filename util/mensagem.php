f75de9c8c8dec17a6ede72325e6c7d09326bd5c0
Mayrton <mayrtondias@hotmail.com>
2014-01-27 21:48:05 -0300

    update mensaguem

---
+++ util/mensagem.php
@@ -0,0 +1,43 @@
+<?php
+    
+    session_start();
+    
+    if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
+        header('location: desconectado.php');
+    }
+    
+    if( isset($_SESSION['mensagem']) == FALSE){
+        header('location: desconectado.php');
+    }else{
+        $tipo=(int)$_SESSION['mensagem'];
+        switch ($tipo){
+            case 1:
+                $men="Cadastro feito com Sucesso!";
+                break;
+            case 2:
+                $men="A decidir";
+                break;
+            case 3:
+                $men="A decidir";
+                break;
+            default :
+                $men=" ";
+                break;
+        }
+        echo $men;
+       
+    }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Guaragas</title>
+    </head>
+    <body>
+        <center>
+            
+            <a href="Home.php" style="border-radius:1em;color:#FFFFFF; font-family:arial; text-decoration:none; padding:5px;background:#1C86EE"><b>OK</b></a>
+        </center>
+    </body>
+</html>

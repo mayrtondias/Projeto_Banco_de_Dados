d08026ee54ac78ef51ac48afefc8008a42843eff
Mayrton <mayrtondias@hotmail.com>
2014-01-27 21:42:12 -0300

    update valida

---
+++ util/valida.php
@@ -0,0 +1,61 @@
+<html>
+    <head>
+        <title>GuaraGas</title>
+    </head>
+   
+    <body>
+        <?php
+        
+            require '../banco/banco.php';
+            $banco=new banco();
+            session_start();
+            
+            $login = $_POST['login'];      
+            $senha = $_POST['senha'];
+            
+            $_SESSION['login']="";
+            $_SESSION['senha']="";
+            
+            $tabela="administrador";
+            $pesquisa="*";
+            
+            $resultado=$banco->pesquisar($pesquisa, $tabela);
+
+            if($resultado==NULL){
+                echo "Problema com a pesquisa! ".pg_errormessage();
+            } else{
+                while($registro = pg_fetch_array($resultado)){
+                    
+                    if(($registro['login']===$login)&&($registro['senha']===$senha)){
+                        $_SESSION['login']=$registro['login'];
+                        $_SESSION['senha']=$registro['senha'];
+                        
+                        header('location: ../administrador/Home.php');
+                    }
+                }
+            }
+            
+            $tabela="funcionario";            
+            $resultado=$banco->pesquisar($pesquisa, $tabela);
+
+            if($resultado==NULL){
+                echo "Problema com a pesquisa! ".pg_errormessage();
+            } else{
+                while($registro = pg_fetch_array($resultado)){
+                    
+                    if(($registro['login']===$login)&&($registro['senha']===$senha)){
+                        $_SESSION['login']=$registro['login'];
+                        $_SESSION['senha']=$registro['senha'];
+                        header('location: ../funcionario/Home.php');
+                    }
+                }
+            }
+            
+            if( ($_SESSION['login']==="")&&($_SESSION['senha']==="")){
+                $_SESSION['login']="erro";
+                $_SESSION['senha']="erro";
+                header('location: indexErro.php');
+            } 
+        ?>
+    </body>
+</html>

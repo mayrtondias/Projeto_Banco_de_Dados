bd137831254221c8bba31446e1693e60452eeee6
Mayrton <mayrtondias@hotmail.com>
2014-01-27 21:50:52 -0300

    update desconectado

---
+++ util/desconectado.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>GuaraGas</title>
+    </head>
+   
+    <body>
+        <center>
+            <div  style="border-radius:1em; width:50%; font-size:19px;background:#99CCFF;color:white" >
+                <center>
+                    <h1>Sua sessão expirou!</h1>
+                </center>
+            </div>
+            <br>
+            <div  style="border-radius:1em; width:30%; font-size:19px;background:#99CCFF;color:white">
+                <center>
+                    <h3><a href="../index.php" style="color:white;text-decoration:none">LogIn</a></h3>
+                </center>
+            </div>
+        </center>    
+    </body>
+</html>
+

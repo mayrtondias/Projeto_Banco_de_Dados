55192c1faeb81df93e0eccf113044cdff2c3dc4d
Mayrton <mayrtondias@hotmail.com>
2014-01-28 22:33:06 -0300

    update Home Estoque

---
+++ administrador/HomeEstoque.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>GuaraGas</title>
+    </head>
+     
+    <body>
+        <?php
+            require 'menuAdministrador.php';
+        ?>
+        
+        <center>
+            <div  style="border-radius:1em; width:20%; font-size:25px; background:#3300FF;float: left;border: 30px">  
+                <center>
+                    <a href="HomeEstoque.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Ajuste        </b></a><br><hr>
+                </center>
+            </div>
+        </center>
+    </body>
+</html>

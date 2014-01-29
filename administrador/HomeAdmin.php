4a6d6e282abddca74f1710aa3061e09af6d78f69
Mayrton <mayrtondias@hotmail.com>
2014-01-28 22:31:53 -0300

    update Home administardor

---
+++ administrador/HomeAdmin.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>GuaraGas</title>
+    </head>
+     
+    <body>
+        <?php
+            require 'menuAdministrador.php';
+        ?>
+        
+        <center>
+            <div  style="border-radius:1em; width:20%; font-size:25px; background:#3300FF;float: left;border: 30px">  
+                <center>
+                    <a href="HomeAdminCad.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cadastrar Administrador         </b></a><br><hr>
+                    <a href="HomeAdminList.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Listar Administradores         </b></a><br><hr>
+                    <a href="HomeFuncDelete.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Excluir Administrador         </b></a><br><hr>
+                    <a href="HomeEstoqueAtualizar.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Atualizar Conta        </b></a>
+                </center>
+            </div>
+        </center>
+    </body>
+</html>

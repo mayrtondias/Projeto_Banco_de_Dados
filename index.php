beacd1776d58a08fc561471e4e0b2dc157797628
Mayrton <mayrtondias@hotmail.com>
2014-01-29 19:57:54 -0300

    update Index

--- index.php
+++ index.php
@@ -12,11 +12,11 @@ and open the template in the editor.
      
     <body>
         <center>
-            <div>
+            <div  style="border-radius:1em; width:20%; font-size:25px; background:#3399FF">
                 <form action="util/valida.php" method="post">
-                    Login:<br>
+                    <label style="color:white; font-family:arial; text-decoration:none; padding:5px">Login:</label><br>
                     <input type="text" name="login"><br>
-                    senha:<br>
+                    <label style="color:white; font-family:arial; text-decoration:none; padding:5px">Senha:</label><br>
                     <input type="password" name="senha"><br>
                     <input type="submit" value="Entrar">
                 </form>

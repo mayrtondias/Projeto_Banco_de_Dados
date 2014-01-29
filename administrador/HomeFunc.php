4597a6b72b7af43ce03502cb906ca9c20614914d
Mayrton <mayrtondias@hotmail.com>
2014-01-28 22:33:59 -0300

    update Home Funcionario

---
+++ administrador/HomeFunc.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>GuaraGas</title>
+    </head>
+     
+    <body>
+        <?php
+            require 'menuAdministrador.php';
+        ?>
+        
+        <center>
+            <div  style="border-radius:1em; width:20%; font-size:25px; background:#3300FF;float: left;border: 30px">  
+                <center>
+                    <a href="HomeFuncCad.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cadastrar Funcionário         </b></a><br><hr>
+                    <a href="HomeFuncList.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Listar Funcionário         </b></a><br><hr>
+                    <a href="HomeFuncDelete.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Excluir Funcionário         </b></a><br><hr>
+                    <a href="HomeFuncAtualizar.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Atualizar Conta        </b></a><br><hr>
+                    
+                </center>
+            </div>
+        </center>
+    </body>
+</html>

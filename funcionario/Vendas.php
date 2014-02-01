8eeeaac9775723d730e6b4918c39cbfaed3b000b
Mayrton <mayrtondias@hotmail.com>
2014-01-31 22:42:47 -0300

    Update Vendas

--- funcionario/Vendas.php
+++ funcionario/Vendas.php
@@ -17,18 +17,10 @@ and open the template in the editor.
         
         <center>
             <div>
-                <form action="validaVenda.php" method="post">
-                    Informe os dos abaixo para cadastrar um novo cliente<br>
-                    Nome:<br>
-                    <input type="text" name="nome"><br>
-                    Contato:<br>
-                    <input type="text" name="contato"><br>
-                    Rua:<br>
-                    <input type="text" name="rua"><br>
-                    Bairro:<br>
-                    <input type="text" name="bairro"><br>
-                    Numero:<br>
-                    <input type="number" name="numero"><br>
+                <form action="vendaPesquisarClienteNome.php" method="post">
+                    
+                    Pesquisar por nome:<br>
+                    <input type="text" name="pesquisarNome"><br>
                     <?php
                         if(isset($_SESSION['erro'])==TRUE){
                             $tipo=(int)$_SESSION['erro'];
@@ -36,20 +28,27 @@ and open the template in the editor.
                                 case 1:
                                     $men="O campo nome não deve ficar em branco<br>nem superior a 30 caracteres<br>";
                                     break;
+                                default :
+                                    $men="";
+                            }
+                            echo $men;
+                        }
+                    ?>
+                    <input type="submit" value="Pesquisar">
+                </form>
+            </div>
+            
+            <div>
+                <form action="vendaPesquisarClienteRua.php" method="post">
+                    
+                    Pesquisar por rua:<br>
+                    <input type="text" name="pesquisarRua"><br>
+                    <?php
+                        if(isset($_SESSION['erro'])==TRUE){
+                            $tipo=(int)$_SESSION['erro'];
+                            switch ($tipo){
                                 case 2:
-                                    $men="O campo contato não deve ficar em branco<br>deve ser nesse formato(xxx)xxxx-xxxx<br>";
-                                    break;
-                                case 3:
                                     $men="O campo da rua não deve ficar em branco<br>nem superior a 50 caracteres.<br>";
-                                    break;
-                                case 4:
-                                    $men="O campo do bairro não deve ficar em branco<br>nem superior a 25 caracteres.<br>";
-                                    break;
-                                case 5:
-                                    $men="O campo do numero não deve ficar em branco<br>Digite um numero valido.<br>";
-                                    break;
-                                case 6:
-                                    $men="Endereço ja cadastrado no sistema<br>";
                                     break;
                                 default :
                                     $men="";
@@ -57,7 +56,7 @@ and open the template in the editor.
                             echo $men;
                         }
                     ?>
-                    <input type="submit" value="Cadastrar">
+                    <input type="submit" value="Pesquisar">
                 </form>
             </div>
         </center>

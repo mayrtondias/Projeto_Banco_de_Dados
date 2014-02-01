eb744b0354db489e73315577706ceae1bebca08e
Mayrton <mayrtondias@hotmail.com>
2014-01-31 23:32:41 -0300

    Update Selecionar produto

--- funcionario/selecionarProduto.php
+++ funcionario/selecionarProduto.php
@@ -1,8 +1,72 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>GuaraGas</title>
+    </head>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    <body>
+        <div>
+            <form method="POST" action="selecionarProduto.php" onsubmit="return confirm('Voce tem certeza?');">
+                <table>
+                    <tr>
+                        <td>Nome</td>
+                        <td>Valor</td>
+                        <td>Codigo</td>
+                        <td>Quantidade Maxima</td>
+                        <td>Selecionar</td>
+                    </tr>
+
+                <?php
+                    require '../banco/banco.php';
+                    
+                    $banco=new banco();
+                    session_start();
+
+                    if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
+                        header('location: ../util/desconectado.php');
+                    }
+                    
+                    
+                    $chave=explode("§", $_POST['selecionado']);
+                    $rua=$chave[0];
+                    $bairro=$chave[1];
+                    $numero=$chave[2];
+
+                    $_SESSION['rua']=$rua;
+                    $_SESSION['bairro']=$bairro;
+                    $_SESSION['numero']=$numero;
+
+                    $tabela="produto";
+                    $pesquisa="*";
+
+                    $resultado=$banco->pesquisar($pesquisa, $tabela);
+
+                    if($resultado==NULL){
+                        echo "Problema na pesquisa<br>";
+                    } else{
+                          while($registro = pg_fetch_array($resultado)){
+                              ?>
+                                <tr>
+                                  <td><?php echo $registro['nome']; ?></td>
+                                  <td><?php echo $registro['valor']; ?></td>
+                                  <td><?php echo $registro['codProduto']; ?></td>
+                                  <td><?php echo $registro['quantidade']; ?></td>
+                                  <td><button type="submit" name="selecionado" value="<?php echo $registro['codProduto']; ?>" >OK</button></td>
+                                </tr>
+                              <?php
+                              }
+                          }
+                ?>
+                </table>
+            </form>
+        </div>
+    </body>
+</html>
+
+
+
+
+
+
 

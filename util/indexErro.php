82f5552bf955f651e29f23a11095233a660ed096
Mayrton <mayrtondias@hotmail.com>
2014-02-01 23:22:46 -0300

    Update indexErro

--- util/indexErro.php
+++ util/indexErro.php
@@ -12,7 +12,7 @@ and open the template in the editor.
      
     <body>
         <center>
-            <div>
+            <div style="border-radius:1em; width:20%; font-size:25px; background:#3399FF">
                 <form action="valida.php" method="post">
                     Login:<br>
                     <input type="text" name="login"><br>

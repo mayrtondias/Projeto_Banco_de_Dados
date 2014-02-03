6e020b28868f27dad06d43f1d23dd87c286313d6
Mayrton <mayrtondias@hotmail.com>
2014-02-02 23:51:34 -0300

    update homeEstoque

--- administrador/HomeEstoque.php
+++ administrador/HomeEstoque.php
@@ -19,7 +19,7 @@ and open the template in the editor.
         <center>
             <div  style="border-radius:1em; width:20%; font-size:25px; background:#3300FF;float: left;border: 30px">  
                 <center>
-                    <a href="HomeEstoque.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Ajuste        </b></a><br><hr>
+                    <a href="cadastraProduto.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cadastrar produto       </b></a><br><hr>
                 </center>
             </div>
         </center>

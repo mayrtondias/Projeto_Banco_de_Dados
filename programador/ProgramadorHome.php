7d8ccef7f5171eb3684f86c7860bd2ca7dea0c6f
Mayrton <mayrtondias@hotmail.com>
2014-02-09 19:45:11 -0300

    update

---
+++ programador/ProgramadorHome.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>GuaraGas</title>
+    </head>
+     
+    <body>
+        
+        <?php
+            
+        ?>
+        <center>
+            <div  style="border-radius:1em; width:20%; font-size:25px; background:#3399FF">
+                <?php
+                    session_start();
+
+                    if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
+                        header('location:util/desconectado.php');
+                    }
+                ?>
+                <form action="ConstruindoBanco.php" method="post">
+                    <input type="submit" value="Cadastrar">
+                </form>
+            </div>
+        </center>
+    </body>
+</html>

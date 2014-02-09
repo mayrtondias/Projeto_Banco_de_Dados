16b1fbfb98a7c525ebea725b64d08111e05e0651
Mayrton <mayrtondias@hotmail.com>
2014-02-09 20:26:16 -0300

    update cadastrafunc

--- administrador/CadastraFunc.php
+++ administrador/CadastraFunc.php
@@ -13,12 +13,12 @@ and open the template in the editor.
      
     <body>
         <?php
-            require 'menuAdministrador.php';
+            require 'MenuAdministrador.php';
         ?>
         
         <center>
             <div>
-                <form action="validaFuncionario.php" method="post">
+                <form action="ValidaFuncionario.php" method="post">
                     Informe os dos abaixo para cadastrar um novo cliente<br>
                     Nome:<br>
                     <input type="text" name="nome"><br>

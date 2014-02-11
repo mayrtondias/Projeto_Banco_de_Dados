57278c4847e92c45a7494364c48aa0e827408c22
Mayrton <mayrtondias@hotmail.com>
2014-02-10 22:00:44 -0300

    update Home Admin

--- administrador/HomeAdmin.php
+++ administrador/HomeAdmin.php
@@ -19,7 +19,7 @@ and open the template in the editor.
         <center>
             <div  style="border-radius:1em; width:20%; font-size:25px; background:#3300FF;border: 30px">  
                 <center>
-                    <a href="HomeAdminCad.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cadastrar Administrador         </b></a><br><hr>
+                    <a href="CadastraAdmin.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cadastrar Administrador         </b></a><br><hr>
                     <a href="HomeAdminList.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Listar Administradores         </b></a><br><hr>
                     <a href="HomeFuncDelete.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Excluir Administrador         </b></a><br><hr>
                     <a href="HomeEstoqueAtualizar.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Atualizar Conta        </b></a>

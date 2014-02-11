640f43b486f1cf8ef95e4c2c545530f09aebc837
Mayrton <mayrtondias@hotmail.com>
2014-02-10 21:43:39 -0300

    update Home

--- administrador/Home.php
+++ administrador/Home.php
@@ -12,7 +12,7 @@ and open the template in the editor.
      
     <body>
         <?php
-            require 'menuAdministrador.php';
+            require 'MenuAdministrador.php';
         ?>
     </body>
 </html>

71d8204f7b575a453c6d62209d72445404ba6d7a
Mayrton <mayrtondias@hotmail.com>
2014-02-10 21:43:51 -0300

    update HomeAdmin

--- administrador/HomeAdmin.php
+++ administrador/HomeAdmin.php
@@ -13,11 +13,11 @@ and open the template in the editor.
      
     <body>
         <?php
-            require 'menuAdministrador.php';
+            require 'MenuAdministrador.php';
         ?>
         
         <center>
-            <div  style="border-radius:1em; width:20%; font-size:25px; background:#3300FF;float: left;border: 30px">  
+            <div  style="border-radius:1em; width:20%; font-size:25px; background:#3300FF;border: 30px">  
                 <center>
                     <a href="HomeAdminCad.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cadastrar Administrador         </b></a><br><hr>
                     <a href="HomeAdminList.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Listar Administradores         </b></a><br><hr>

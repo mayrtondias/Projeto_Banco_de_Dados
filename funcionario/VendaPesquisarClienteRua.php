83ad83443889a45a38e5069f7d3e8afc2149e273
Mayrton <mayrtondias@hotmail.com>
2014-02-10 22:12:06 -0300

    update VendaPesquisarClienteRua

--- funcionario/VendaPesquisarClienteRua.php
+++ funcionario/VendaPesquisarClienteRua.php
@@ -1,73 +1,72 @@
 <!DOCTYPE html>
-<?php
-    ?>
-    <html>
-        <head>
-            <meta charset="UTF-8">
-            <title>GuaraGas</title>
-        </head>
 
-        <body>
-            <div>
-                <form method="POST" action="selecionarProduto.php" onsubmit="return confirm('Voce tem certeza?');">
-                    <table>
-                        <tr>
-                            <td>Rua</td>
-                            <td>Bairro</td>
-                            <td>Rua</td>
-                            <td>Contato</td>
-                            <td>Selecionar</td>
-                        </tr>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>GuaraGas</title>
+    </head>
 
-                    <?php
-                    
-                        require '../banco/banco.php';
+    <body>
+        <div>
+            <form method="POST" action="SelecionarProduto.php" onsubmit="return confirm('Voce tem certeza?');">
+                <table>
+                    <tr>
+                        <td>Rua</td>
+                        <td>Bairro</td>
+                        <td>Rua</td>
+                        <td>Contato</td>
+                        <td>Selecionar</td>
+                    </tr>
 
-                        $banco=new banco();
-                        session_start();
+                <?php
 
-                        if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
-                            header('location: ../util/desconectado.php');
-                        }
+                    require '../banco/banco.php';
 
-                        $rua = $_POST['pesquisarRua'];      
-                        $_SESSION['erro']="";
-                        $tabela="cliente";
-                        $pesquisa="*";
-                        
-                        if(($nome==="")||(strlen($nome)>50)){
-                            $_SESSION['erro']="2";
-                            header('location: Vendas.php');
-                        }
-    
-                        $resultado=$banco->pesquisar($pesquisa, $tabela);
-                        
-                        if($resultado==NULL){
-                            echo "Problema na pesquisa<br>";
-                        } else{
-                              while($registro = pg_fetch_array($resultado)){
-                                  
-                                  if($registro['rua']===$rua){
-                                      ?>
-                                      <tr>
-                                        <td><?php echo $registro['rua']; ?></td>
-                                        <td><?php echo $registro['bairro']; ?></td>
-                                        <td><?php echo $registro['numero']; ?></td>
-                                        <td><?php echo $registro['contato']; ?></td>
-                                        <td><button type="submit" name="selecionado" value="<?php echo $registro['rua']."§".$registro['bairro']."§".$registro['numero']; ?>" >OK</button></td>
-                                    </tr>
-                                    <?php
-                                  }
+                    $banco=new banco();
+                    session_start();
+
+                    if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
+                        header('location: ../util/desconectado.php');
+                    }
+
+                    $rua = $_POST['pesquisarRua'];      
+                    $_SESSION['erro']="";
+                    $tabela="cliente";
+                    $pesquisa="*";
+
+                    if(($nome==="")||(strlen($nome)>50)){
+                        $_SESSION['erro']="2";
+                        header('location: Vendas.php');
+                    }
+
+                    $resultado=$banco->pesquisar($pesquisa, $tabela);
+
+                    if($resultado==NULL){
+                        echo "Problema na pesquisa<br>";
+                    } else{
+                          while($registro = pg_fetch_array($resultado)){
+
+                              if($registro['rua']===$rua){
+                                  ?>
+                                  <tr>
+                                    <td><?php echo $registro['rua']; ?></td>
+                                    <td><?php echo $registro['bairro']; ?></td>
+                                    <td><?php echo $registro['numero']; ?></td>
+                                    <td><?php echo $registro['contato']; ?></td>
+                                    <td><button type="submit" name="selecionado" value="<?php echo $registro['rua']."§".$registro['bairro']."§".$registro['numero']; ?>" >OK</button></td>
+                                </tr>
+                                <?php
                               }
-                         }
+                          }
+                     }
 
 
-                ?>
-                    </table>
-                </form>
-            </div>
-        </body>
-    </html>
+            ?>
+                </table>
+            </form>
+        </div>
+    </body>
+</html>
 
 
 

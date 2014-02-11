e6c25cc7142f2f4d07adf41190eb2ec24cd50c80
Mayrton <mayrtondias@hotmail.com>
2014-02-10 21:45:00 -0300

    update HomeEstoque

--- administrador/HomeEstoque.php
+++ administrador/HomeEstoque.php
@@ -13,13 +13,13 @@ and open the template in the editor.
      
     <body>
         <?php
-            require 'menuAdministrador.php';
+            require 'MenuAdministrador.php';
         ?>
         
         <center>
-            <div  style="border-radius:1em; width:20%; font-size:25px; background:#3300FF;float: left;border: 30px">  
+            <div  style="border-radius:1em; width:20%; font-size:25px; background:#3300FF;border: 30px">  
                 <center>
-                    <a href="cadastraProduto.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cadastrar produto       </b></a><br><hr>
+                    <a href="CadastraProduto.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cadastrar produto       </b></a><br><hr>
                 </center>
             </div>
         </center>

e96d08ea14a6c4bd99d611b2a0a5b28c24f9e2b2
Mayrton <mayrtondias@hotmail.com>
2014-02-10 21:45:18 -0300

    update HomeFinacas

--- administrador/HomeFinancas.php
+++ administrador/HomeFinancas.php
@@ -13,11 +13,11 @@ and open the template in the editor.
      
     <body>
         <?php
-            require 'menuAdministrador.php';
+            require 'MenuAdministrador.php';
         ?>
         
         <center>
-            <div  style="border-radius:1em; width:20%; font-size:25px; background:#3300FF;float: left;border: 30px;padding-left: 30px">  
+            <div  style="border-radius:1em; width:20%; font-size:25px; background:#3300FF;border: 30px;padding-left: 30px">  
                 <center>
                     <a href="HomeFinancas.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Ajuste         </b></a><br><hr>
                 </center>

e0484ad86977a63b266a13d1e895df9c9ccadfb8
Mayrton <mayrtondias@hotmail.com>
2014-02-12 23:47:54 -0300

    create Func

--- administrador/HomeFunc.php
+++ administrador/HomeFunc.php
@@ -20,7 +20,7 @@ and open the template in the editor.
             <div  style="border-radius:1em; width:20%; font-size:25px; background:#3300FF;border: 30px">  
                 <center>
                     <a href="CadastraFunc.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cadastrar Funcionário         </b></a><br><hr>
-                    <a href="HomeAdminList.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Listar Funcionário         </b></a><br><hr>
+                    <a href="HomeFuncList.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Listar Funcionário         </b></a><br><hr>
                     <a href="HomeFuncDelete.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Excluir Funcionário         </b></a><br><hr>
                     <a href="HomeFuncAtualizar.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Atualizar Conta        </b></a><br><hr>
                     

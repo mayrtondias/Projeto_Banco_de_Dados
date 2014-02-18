4673f7dd2e2e219896b8ecbfaa8fb5ead6edff08
Mayrton <mayrtondias@hotmail.com>
2014-02-18 00:04:32 -0300

    update

--- util/Sair.php
+++ util/Sair.php
@@ -20,7 +20,7 @@
     <body>
         <center>
             <p style="font-family: arial; font-size: 16px;color:#1C86EE"><b>Usuario desconectado!</b></p>
-            <a href="../index.php" style="border-radius:1em;color:#FFFFFF; font-family:arial; text-decoration:none; padding:5px;background:#1C86EE"><b>Voltar ao Sistema</b></a>
+            <a href="../index.php" style="border-radius:1em;color:#FFFFFF; font-family:arial; text-decoration:none; padding:5px;background:#1C86EE"><b>Voltar ao Sistema </b></a>
         </center>
     </body>
 </html>

48944937a3b7b6653ced33475cd4df7fbc8920ff
Mayrton <mayrtondias@hotmail.com>
2014-02-23 21:48:04 -0300

    update

--- util/Sucesso.php
+++ util/Sucesso.php
@@ -4,9 +4,9 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Guaragas</title>
     </head>
+    
     <body>
         <center>
-            
             <a href="../programador/ProgramadorHome.php" style="border-radius:1em;color:#FFFFFF; font-family:arial; text-decoration:none; padding:5px;background:#1C86EE"><b>Operação realizada com Sucesso!</b></a>
         </center>
     </body>

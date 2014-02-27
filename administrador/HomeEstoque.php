2757bc6cfa194296d2529777ae0b5e91b2e439e4
Mayrton <mayrtondias@hotmail.com>
2014-02-26 23:15:00 -0300

    update

--- administrador/HomeEstoque.php
+++ administrador/HomeEstoque.php
@@ -12,7 +12,7 @@
         ?>
         
         <center>
-            <div  style="border-radius:1em; width:20%; font-size:25px; background:#3300FF;border: 30px">  
+            <div  style="border-radius:1em; width:20%; font-size:25px; background:#3300FF; border: 30px">  
                 <center>
                     <a href="CadastraProduto.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cadastrar produto       </b></a><br><hr>
                 </center>

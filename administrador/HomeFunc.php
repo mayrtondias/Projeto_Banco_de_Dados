7276b75538e80b7d565c697da6466b912da521bf
Mayrton <mayrtondias@hotmail.com>
2014-02-26 23:15:14 -0300

    update

--- administrador/HomeFunc.php
+++ administrador/HomeFunc.php
@@ -12,7 +12,7 @@
         ?>
         
         <center>
-            <div  style="border-radius:1em; width:20%; font-size:25px; background:#3300FF;border: 30px">  
+            <div  style="border-radius:1em; width:20%; font-size:25px; background:#3300FF; border: 30px">  
                 <center>
                     <a href="CadastraFunc.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cadastrar Funcionário         </b></a><br><hr>
                     <a href="HomeFuncList.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Listar Funcionário         </b></a><br><hr>

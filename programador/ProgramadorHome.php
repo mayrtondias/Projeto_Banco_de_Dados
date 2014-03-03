7ab407bd5bd30cd4686684f0a5a8171a378d1846
Mayrton <mayrtondias@hotmail.com>
2014-03-02 23:35:36 -0300

    criate

--- programador/ProgramadorHome.php
+++ programador/ProgramadorHome.php
@@ -7,8 +7,15 @@
     </head>
      
     <body>
+        <!-- logout!-->
+        <div style="border-radius:1em; width:25%; font-size:12px;background:#33aa00; float: right; border-bottom: 5px">
+            <center>
+                <a href="../util/sair.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>Olá <?php echo $_SESSION['login'];?>, <i style="">Logout</i></b></a><br>
+            </center>
+        </div>
+        </br>
         <center>
-            <div  style="border-radius:1em; width:20%; font-size:25px; background:#3399FF">
+            <div  style="border-radius:1em; width:20%; font-size:25px; background:#33aa00; margin: 10px; padding: 20px">
                 <?php
                     session_start();
                     if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){

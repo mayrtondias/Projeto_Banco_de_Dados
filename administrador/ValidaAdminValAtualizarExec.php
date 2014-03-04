df7ea5619d2013b33e73c6a823d8962794b27daa
Mayrton <mayrtondias@hotmail.com>
2014-03-03 22:08:46 -0300

    update

--- administrador/ValidaAdminValAtualizarExec.php
+++ administrador/ValidaAdminValAtualizarExec.php
@@ -1,8 +1,52 @@
 <?php
+    
+    require '../banco/Banco.php';
+    
+    $banco=new banco();
+    session_start();
+    
+    if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
+        header('location: ../util/desconectado.php');
+    }
+            
+    $nome = $_POST['nome'];      
+    $login = $_POST['login'];
+    $senha = $_POST['senha'];      
+    $administradorCadastrado="f";
+    $_SESSION['erro']="";
+    
+    $tabela="administrador";
+    $pesquisa="*";
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    $resultado=$banco->pesquisar($pesquisa, $tabela);
 
+    if($resultado==NULL){
+        echo "Problema na pesquisa.<br>";
+    } else{
+          while($registro = pg_fetch_array($resultado)){
+              if($registro['login']===$nome){
+                  $administradorCadastrado="t";
+              }
+          }
+     }
+ 
+    if(($nome==="")||(strlen($nome)>50)){
+        $_SESSION['erro']="1";
+    } else if(($login==="")||(strlen($login)>15)){
+        $_SESSION['erro']="2";
+    } else if(($senha==="")||(strlen($senha)>15)||(strlen($senha)<8)){
+        $_SESSION['erro']="3";
+    } else if($clienteCadastrado==="t"){
+        $_SESSION['erro']="4";
+    } 
+    
+    if($_SESSION['erro'] === ""){
+        $banco->inserirAdministrador($nome, $login, $senha);
+        $banco->update($tabela, $clausuraSET, $clausuraWere);
+        unset($_SESSION['erro']);
+        $_SESSION['mensagem']="3";
+        header('location: mensagem.php');
+    }else{
+        header('location: HomeAdminValAtualizar.php');
+    }
+?>

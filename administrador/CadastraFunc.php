9260a5b114615faa9cb0bcf8c6db7376e7da551b
Mayrton <mayrtondias@hotmail.com>
2014-03-04 23:08:22 -0300

    update

--- administrador/CadastraFunc.php
+++ administrador/CadastraFunc.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 
-
 <html>
     <head>
         <meta charset="UTF-8">
@@ -13,7 +12,7 @@
         ?>
         
         <center>
-            <div>
+            <div style="border-radius:1em; width:20%; font-size:25px; background:#3300FF; border: 30px">
                 <form action="ValidaFuncionario.php" method="post">
                     Informe os dos abaixo para cadastrar um novo cliente<br>
                     Nome:<br>
@@ -21,7 +20,7 @@
                     Login:<br>
                     <input type="text" name="login"><br>
                     Senha:<br>
-                    <input type="text" name="senha"><br>
+                    <input type="password" name="senha"><br>
                     Identidade:<br>
                     <input type="text" name="identidade"><br>
                     CPF:<br>
@@ -37,7 +36,7 @@
                             $tipo=(int)$_SESSION['erro'];
                             switch ($tipo){
                                 case 1:
-                                    $men="O campo nome não deve ficar em branco<br>nem superior a 50 caracteres<br>";
+                                    $men="O campo nome não deve ficar em branco<br>nem superior a 30 caracteres<br>";
                                     break;
                                 case 2:
                                     $men="O campo Login não deve ficar em branco<br>nem superior a 15 caracteres<br>nem inferior à 8 caracteres<br>";
@@ -46,7 +45,28 @@
                                     $men="O campo da senha não deve ficar em branco<br>nem superior a 15 caracteres.<br>";
                                     break;
                                 case 4:
-                                    $men="Administrador ja cadastrado no sistema<br>";
+                                    $men="O campo da identidade não deve ficar em branco<br>nem superior a 15 caracteres.<br>";
+                                    break;
+                                case 5:
+                                    $men="O campo do CPF não deve ficar em branco<br>nem superior a 14 caracteres<br>O formato desejado é xxx.xxx.xxx-xx<br>";
+                                    break;
+                                case 6:
+                                    $men="O campo do salário não deve ficar em branco<br>e seu valor não deverá ser negativo<br>";
+                                    break;
+                                case 7:
+                                    $men="O campo do Telefone não deve ficar em branco<br>nem superior a 14 caracteres<br>O formato desejado é (xx)xxxx-xxxx<br>";
+                                    break;
+                                case 8:
+                                    $men="O campo Cargo não deve ficar em branco<br>nem superior a 20 caracteres<br>";
+                                    break;
+                                case 9:
+                                    $men="Funcionário ja cadastrado no sistema<br>";
+                                    break;
+                                case 10:
+                                    $men="Usuário ja cadastrado no sistema,<br>Informe um login distinto.<br>";
+                                    break;
+                                case 11:
+                                    $men="O CPF desse funcionario ja esta cadastrado no sistema,<br>Informe um CPF distinto.<br>";
                                     break;
                                 default :
                                     $men="";

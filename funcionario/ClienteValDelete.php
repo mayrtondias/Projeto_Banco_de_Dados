8d868e2f2eaf03aa7da3ac0b08aa7b6aa1d37a74
Mayrton <mayrtondias@hotmail.com>
2014-03-08 23:58:06 -0300

    update

--- funcionario/ClienteValDelete.php
+++ funcionario/ClienteValDelete.php
@@ -1,8 +1,28 @@
 <?php
+    require '../banco/Banco.php';
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    $banco=new banco();
+    session_start();
 
+    if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
+        header('location: ../util/desconectado.php');
+    }
+
+    $chave=explode("§", $_POST['selecionado']);
+                    $rua=$chave[0];
+                    $bairro=$chave[1];
+                    $numero=$chave[2];
+    
+    $tabela="cliente";
+    $clausuraWere="rua='$rua' AND bairro= '$bairro' AND numero='$numero'";
+
+    $resultado=$banco->deletar($tabela, $clausuraWere);
+    
+    if($resultado==NULL){
+        echo "Problema na exclusão.<br>";
+    }else{
+        $_SESSION['mensagem']="2";
+        header('location: mensagem.php');
+        
+    }
+    ?>

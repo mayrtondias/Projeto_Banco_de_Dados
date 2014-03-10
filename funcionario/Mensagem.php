cf93977f7ec05dd6d9c9d0c4c0ecd97dbb9e1e14
Mayrton <mayrtondias@hotmail.com>
2014-03-09 22:59:33 -0300

    update

--- funcionario/Mensagem.php
+++ funcionario/Mensagem.php
@@ -1,33 +1,3 @@
-<?php
-    
-    session_start();
-    
-    if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
-        header('location: ../util/desconectado.php');
-    }
-    
-    if( isset($_SESSION['mensagem']) == FALSE){
-        header('location: ../util/desconectado.php');
-    }else{
-        $tipo=(int)$_SESSION['mensagem'];
-        switch ($tipo){
-            case 1:
-                $men="Cadastro feito com Sucesso! ";
-                break;
-            case 2:
-                $men="A decidir.";
-                break;
-            case 3:
-                $men="A decidir.";
-                break;
-            default :
-                $men=" ";
-                break;
-        }
-        echo $men;
-       
-    }
-?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -36,8 +6,40 @@
     </head>
     <body>
         <center>
+            <?php
+    
+            session_start();
+
+            if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
+                header('location: ../util/desconectado.php');
+            }
+
+            if( isset($_SESSION['mensagem']) == FALSE){
+                header('location: ../util/desconectado.php');
+            }else{
+                $tipo=(int)$_SESSION['mensagem'];
+                switch ($tipo){
+                    case 1:
+                        echo "Cadastro feito com Sucesso!";
+                        ?>
+                            <a href="Home.php" style="border-radius:1em;color:#FFFFFF; font-family:arial; text-decoration:none; padding:5px;background:#1C86EE"><b>OK</b></a>
+                        <?php
+                        break;
+                    case 2:
+                        ?>
+                            <a href="ClienteDelete.php" style="border-radius:1em;color:#FFFFFF; font-family:arial; text-decoration:none; padding:5px;background:#1C86EE"><b>OK</b></a>
+                        <?php
+                        break;
+                    default :
+                        ?>
+                            <a href="Home.php" style="border-radius:1em;color:#FFFFFF; font-family:arial; text-decoration:none; padding:5px;background:#1C86EE"><b>OK</b></a>
+                        <?php
+                        break;
+                }
+            }
+        ?>
+
             
-            <a href="Home.php" style="border-radius:1em;color:#FFFFFF; font-family:arial; text-decoration:none; padding:5px;background:#1C86EE"><b>OK</b></a>
         </center>
     </body>
 </html>

e5d7b10fef8288ef9a1da7ad1ad98e6fbf29baaf
Mayrton <mayrtondias@hotmail.com>
2014-03-15 20:44:03 -0300

    update

--- administrador/HomeAdminDelete.php
+++ administrador/HomeAdminDelete.php
@@ -4,14 +4,16 @@
     <head>
         <meta charset="UTF-8">
         <title>GuaraGas</title>
+        <link rel="stylesheet" type="text/css" href="../estilos/Padrao.css" media="all"/>
     </head>
 
     <body>
+        
         <?php
             require 'MenuAdministrador.php';
         ?>
         <center>
-            <div style="border-radius:1em; width:20%; font-size:25px; background:#3300FF; border: 30px">
+            <div id="tres">
                 <form method="POST" action="HomeAdminValDelete.php" >
                     <?php
 

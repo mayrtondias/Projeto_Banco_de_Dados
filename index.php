0484a01857053d550337ba2a0fa9dbb5c45ffe10
Mayrton <mayrtondias@hotmail.com>
2014-03-14 22:47:14 -0300

    update

--- index.php
+++ index.php
@@ -3,19 +3,25 @@
     <head>
         <meta charset="UTF-8">
         <title>Guaragas</title>
+        <link rel="stylesheet" type="text/css" href="estilos/Padrao.css" media="all"/>
     </head>
-     
+
     <body>
-        <center>
-            <div  style="border-radius:1em; width:20%; font-size:25px; background:#3399FF; margin: 15%">
-                <form action="util/valida.php" method="post">
-                    <label style="color:white; font-family:arial; text-decoration:none; padding:5px">Login:</label><br>
-                    <input type="text" name="login"><br>
-                    <label style="color:white; font-family:arial; text-decoration:none; padding:5px">Senha:</label><br>
-                    <input type="password" name="senha"><br>
-                    <input type="submit" value="Entrar">
-                </form>
-            </div>
-        </center>
-    </body>
+        <h1 id="um">GuaraGás - Satisfazendo pessoas em toda João Pessoa</h1>
+    <center>
+        <div id="tres">
+            <form action="util/valida.php" method="post">
+                <label>Login:</label><br>
+                <input type="text" name="login"><br>
+                <label>Senha:</label><br>
+                <input type="password" name="senha"><br>
+                <input type="submit" value="Entrar">
+            </form>
+        </div>
+        <div id="dois">
+            Copyright © 2014 by Fellipe Mahon, Mayrton Dias e Vitor Soares.</br>
+            Todos os direitos reservados.
+        </div>
+    </center>
+</body>
 </html>

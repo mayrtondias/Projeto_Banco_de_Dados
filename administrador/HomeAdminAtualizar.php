052f660c244e32652c8a969eda4eab121d394f59
Mayrton <mayrtondias@hotmail.com>
2014-03-15 20:43:55 -0300

    update

--- administrador/HomeAdminAtualizar.php
+++ administrador/HomeAdminAtualizar.php
@@ -4,14 +4,16 @@
     <head>
         <meta charset="UTF-8">
         <title>GuaraGas</title>
+        <link rel="stylesheet" type="text/css" href="../estilos/Padrao.css" media="all"/>
     </head>
 
     <body>
+        
         <?php
             require 'MenuAdministrador.php';
         ?>
         <center>
-            <div style="border-radius:1em; width:40%; font-size:25px; background:#3300FF; border: 30px">
+            <div id="tres">
                 <form method="POST" action="HomeAdminValAtualizar.php" >
                     <?php
 
@@ -87,6 +89,6 @@
                  ?>
              </form>
         </div>
-            </center>
+    </center>
     </body>
 </html>

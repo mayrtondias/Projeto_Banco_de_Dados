5e5bb8e005b8331188ff9219a1549e92c09aba87
Mayrton <mayrtondias@hotmail.com>
2014-03-15 14:38:59 -0300

    update

--- administrador/Home.php
+++ administrador/Home.php
@@ -4,11 +4,15 @@
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link rel="stylesheet" type="text/css" href="../estilos/Padrao.css" media="all"/>
     </head>
-     
+
     <body>
         <?php
             require 'MenuAdministrador.php';
         ?>
-    </body>
+    <center>
+        
+    </center>
+</body>
 </html>

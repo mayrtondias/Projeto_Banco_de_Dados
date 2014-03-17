d147f4d2bb74dfc8fa1a570d1f8bae4ac7176eb5
Mayrton <mayrtondias@hotmail.com>
2014-03-17 07:25:16 -0300

    update

---
+++ administrador/FornecedorValDelete.php
@@ -0,0 +1,25 @@
+<?php
+    require '../banco/Banco.php';
+
+    $banco=new banco();
+    session_start();
+
+    if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
+        header('location: ../util/desconectado.php');
+    }
+
+    $chave=$_POST['chave'];
+    
+    $tabela="fornecedor";
+    $clausuraWere="nome = '$chave'";
+
+    $resultado=$banco->deletar($tabela, $clausuraWere);
+    
+    if($resultado==NULL){
+        echo "Problema na exclusão.<br>";
+    }else{
+        $_SESSION['mensagem']="1";
+        header('location: mensagem.php');
+        
+    }
+    ?>

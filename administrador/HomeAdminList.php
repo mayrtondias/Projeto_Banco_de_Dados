4f46600a55305375fd13d188fb7d8205c8e95f67
Mayrton <mayrtondias@hotmail.com>
2014-03-16 23:07:42 -0300

    update

--- administrador/HomeAdminList.php
+++ administrador/HomeAdminList.php
@@ -8,83 +8,85 @@
     </head>
 
     <body>
-        
+
         <?php
-            require 'MenuAdministrador.php';
+        require 'MenuAdministrador.php';
         ?>
-        <center>
-            <div id="tres">
-                <form method="POST" action="HomeAdminList.php" >
-                    <?php
+    <center>
+        <div id="tres">
+            <form method="POST" action="HomeAdminList.php" >
+                <?php
+                require '../banco/Banco.php';
 
-                    require '../banco/Banco.php';
+                $banco = new banco();
+                session_start();
 
-                    $banco=new banco();
-                    session_start();
+                if (( isset($_SESSION['login']) == FALSE) || ( isset($_SESSION['senha']) == FALSE)) {
+                    header('location: ../util/desconectado.php');
+                }
 
-                    if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
-                        header('location: ../util/desconectado.php');
-                    }
-                    
-                    $tabela="administrador";
-                    $pesquisa="*";
-                       
-                    $resultado=$banco->pesquisar($pesquisa, $tabela);
+                $tabela = "administrador";
+                $pesquisa = "*";
 
-                    if($resultado==NULL){
-                        echo "Problema na pesquisa<br>";
-                    } else{
-                        ?>
-                <table>
-                    <tr>
-                        <td></td>
-                        <td>Nome</td>
-                        <td>Login</td>
-                    </tr>
+                $resultado = $banco->pesquisar($pesquisa, $tabela);
+
+                if ($resultado == NULL) {
+                    echo "Problema na pesquisa<br>";
+                } else {
+                    ?>
+                    <table>
+                        <tr>
+                            <td></td>
+                            <td>Nome</td>
+                            <td>Login</td>
+                        </tr>
                         <?php
-                        $contador=1;
+                        $contador=0;
                         if( isset($_POST['selecionado'])==false){
                             $qtdeResult=0;
-                        }else if(($_POST['selecionado']==="anterior")||($qtdeResult>1)){
+                        }else {
+                            if( isset($_POST['pagina'])==true){
+                                $qtdeResult=$_POST['pagina'];
+                            }
+                            if(($_POST['selecionado']==="anterior")||($qtdeResult>1)){
                                 --$qtdeResult;
-                        }else if($_POST['selecionado']==="proximo"){
-                                ++$qtdeResult;
-                        }else $qtdeResult=0;
+                            }else if($_POST['selecionado']==="proximo"){
+                                    ++$qtdeResult;
+                            }else $qtdeResult=0;
+                        }
+                        while ($registro = pg_fetch_array($resultado)) {
 
-                        while($registro = pg_fetch_array($resultado)){
-                              
-                            if(($contador>=($qtdeResult*10))&&($contador<($qtdeResult*10+10))){
-                                  ?>
-                                  <tr>
-                                    <td><?php echo $contador; ?></td>
+                            if (($contador >= ($qtdeResult * 10)) && ($contador < ($qtdeResult * 10 + 10))) {
+                                ?>
+                                <tr>
+                                    <td><?php echo ($contador+1); ?></td>
                                     <td><?php echo $registro['nome']; ?></td>
                                     <td><?php echo $registro['login']; ?></td>
-                                  </tr>
+                                </tr>
                                 <?php
-                                
-                              }
-                              ++$contador;
-                          }
-                          
-                          if($qtdeResult>0){
-                              ?>
-                              <td><button type="submit" name="selecionado" value="anterior" >Anterior</button></td>
-                              <?php
-                              
-                          }
-                          
-                          if((($contador/10)>=($qtdeResult+1))&&(($contador%10)!=0)){
-                              ?>
-                              <td><button type="submit" name="selecionado" value="proximo" >Próximo</button></td>
-                              <?php
-                          }
-                          
-                      }
-            ?>
+                            }
+                            ++$contador;
+                        }
+                    }
+                    ?>
                 </table>
+                    <?php
+                    if ($qtdeResult > 0) {
+                        ?>
+                        <button type="submit" name="selecionado" value="anterior" >Anterior</button>
+                    <?php
+                    }
+
+                    if ((($contador / 10) >= ($qtdeResult + 1)) && (($contador % 10) != 0)) {
+                        ?>
+                        <button type="submit" name="selecionado" value="proximo" >Próximo</button>
+                        <?php
+                    }
+                ?>
+                <input type="hidden" name="pagina" value="<?php echo $qtdeResult; ?>"><br>
             </form>
         </div>
-      </center>
-    </body>
+    </center>
+</body>
 </html>
 

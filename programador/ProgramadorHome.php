8c11c8c6a16f3477561ac073cc27b853175a29c6
Mayrton <mayrtondias@hotmail.com>
2014-03-16 21:31:45 -0300

    update

--- programador/ProgramadorHome.php
+++ programador/ProgramadorHome.php
@@ -4,18 +4,19 @@
     <head>
         <meta charset="UTF-8">
         <title>GuaraGas</title>
+        <link rel="stylesheet" type="text/css" href="../estilos/Padrao.css" media="all"/>
     </head>
      
     <body>
         <!-- logout!-->
-        <div style="border-radius:1em; width:25%; font-size:12px;background:#33aa00; float: right; border-bottom: 5px">
+        <div style="border-radius:1em; width:25%; font-size:12px;background:#0000f0; float: right; border-bottom: 5px">
             <center>
-                <a href="../util/sair.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>Olá <?php echo $_SESSION['login'];?>, <i style="">Logout</i></b></a><br>
+                <a href="../util/sair.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b><i style="">Logout</i></b></a><br>
             </center>
         </div>
         </br>
         <center>
-            <div  style="border-radius:1em; width:20%; font-size:25px; background:#33aa00; margin: 10px; padding: 20px">
+            <div  id="tres" style="width: 50%;padding: 5px">
                 <?php
                     session_start();
                     if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
@@ -23,11 +24,33 @@
                     }
                 ?>
                 <form action="ConstruindoBanco.php" method="post">
-                    <button type="submit" name="selecionado" value="criarBanco">Criar Banco</button>
-                    <button type="submit" name="selecionado" value="criarTabelas">Criar Tabelas</button>
-                    <button type="submit" name="selecionado" value="detetarBanco">Detetar Banco</button>
-                    <button type="submit" name="selecionado" value="detetarTabelas">Detetar Tabelas</button>
-                    <button type="submit" name="selecionado" value="inserirAdministrador">Inserir Administrador padrão</button>
+                    <button type="submit" name="selecionado" value="1">Criar Banco</button>
+                    <button type="submit" name="selecionado" value="2">Criar Tabela Administrador</button>
+                    <button type="submit" name="selecionado" value="3">Criar Tabela Cliente</button>
+                    <button type="submit" name="selecionado" value="4">Criar Tabela Funcionario</button>
+                    <button type="submit" name="selecionado" value="5">Criar Tabela Produto</button>
+                    <button type="submit" name="selecionado" value="6">Criar Tabela Venda</button>
+                    <button type="submit" name="selecionado" value="8">Criar Tabela Fornecedor</button>
+                    <button type="submit" name="selecionado" value="7">Criar Tabela Compra</button>
+                    <button type="submit" name="selecionado" value="9">Criar Tabela Conta</button>
+                    <button type="submit" name="selecionado" value="10">Apagar Banco</button>
+                    <button type="submit" name="selecionado" value="11">Criar Indices Administrador</button>
+                    <button type="submit" name="selecionado" value="12">Criar Indices Cliente</button>
+                    <button type="submit" name="selecionado" value="15">Criar Indices Fornecedor</button>
+                    <button type="submit" name="selecionado" value="13">Criar Indices Compra</button>
+                    <button type="submit" name="selecionado" value="14">Criar Indices Conta</button>
+                    <button type="submit" name="selecionado" value="16">Criar Indices Funcionario</button>
+                    <button type="submit" name="selecionado" value="17">Criar Indices Produto</button>
+                    <button type="submit" name="selecionado" value="18">Criar Indices Venda</button>
+                    <button type="submit" name="selecionado" value="19">Apagar Tabela Administrador</button>
+                    <button type="submit" name="selecionado" value="20">Apagar Tabela Cliente</button>
+                    <button type="submit" name="selecionado" value="21">Apagar Tabela Compra</button>
+                    <button type="submit" name="selecionado" value="22">Apagar Tabela Conta</button>
+                    <button type="submit" name="selecionado" value="23">Apagar Tabela Fornecedor</button>
+                    <button type="submit" name="selecionado" value="24">Apagar Tabela Funcionario</button>
+                    <button type="submit" name="selecionado" value="25">Apagar Tabela Produto</button>
+                    <button type="submit" name="selecionado" value="26">Apagar Tabela Venda</button>
+                    <button type="submit" name="selecionado" value="27">Inserir Administrador padrão</button>
                 </form>
             </div>
         </center>

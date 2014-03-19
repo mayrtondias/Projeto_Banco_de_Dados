f00f0b1fced963e9e1845994a3bf526da5d28aad
Mayrton <mayrtondias@hotmail.com>
2014-03-19 14:51:36 -0300

    update

--- funcionario/MenuFuncionario.php
+++ funcionario/MenuFuncionario.php
@@ -1,25 +1,38 @@
-
 <?php
     session_start();
 
     if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
         header('location: ../util/desconectado.php');
     }
+    //<h1 id="banner">GuaraGás - Satisfazendo pessoas em toda João Pessoa</h1>
 ?>
-    <div  style="border-radius:1em; width:20%; font-size:25px; background:#3399FF; float: left">  
-        <center>
-            <a href="Home.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Home         </b></a><br><hr>
-            <a href="Vendas.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Realizar Vendas         </b></a><br><hr>
-            <a href="HomeCliente.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cliente         </b></a><br><hr>
-            <a href="Entrega.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Produtos para Entrega        </b></a><br><hr>
-            <a href="VendasDiaria.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Vendas Diaria        </b></a><br><hr>
-            <a href="../util/sair.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Sair         </b></a><br>
-        </center>
-    </div>
-    <!-- logout! -->
-    <div  style="border-radius:1em; width:25%; font-size:12px;background:#00BFFF; float: right; border-bottom: 5px">
-        <center>
-            <a href="../util/sair.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>Olá <?php echo $_SESSION['login'];?>, <i style="">Logout</i></b></a><br>
-        </center>
-    </div>
+    <center>
+        
+        <h1 id="banner">GuaraGás - Satisfazendo pessoas em toda João Pessoa</h1>
+
+        <div id="conteudo">
+            <ul id="menu">
+                <li><a href="Home.php"><b> Home </b></a><br>						
+                <li><b> Realizar Vendas </b>
+                    <ul>
+                        <li><a href="Vendas.php">Realizar Venda</a></li>								
+                    </ul>
+                <li><b> Gerenciar Clientes </b>
+                    <ul>
+                        <li><a href="ClienteCadastrar.php">Cadastrar Cliente</a></li>
+                        <li><a href="ClienteList.php">Listar Cliente</a></li>
+                        <li><a href="ClienteAtualizar.php">Atualizar Cliente</a></li>
+                        <li><a href="ClienteDelete.php">Excluir Cliente</a></li>								
+                    </ul>
+                
+                <li><a href="../util/sair.php"><b> Sair </b></a><br></li>							
+            </ul>
+
+        </div> 	
+
+        <div id="dois">
+            Copyright © 2014 by Fellipe Mahon, Mayrton Dias e Vitor Soares.</br>
+            Todos os direitos reservados.
+        </div>
+    </center>
     <br>

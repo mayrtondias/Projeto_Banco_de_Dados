adfaeced04e61067b1f00bc9a14d9b5cc1431094
Mayrton <mayrtondias@hotmail.com>
2014-03-19 14:51:32 -0300

    update

--- funcionario/Home.php
+++ funcionario/Home.php
@@ -4,6 +4,7 @@
     <head>
         <meta charset="UTF-8">
         <title>GuaraGas </title>
+        <link rel="stylesheet" type="text/css" href="../estilos/Padrao.css" media="all"/>
     </head>
      
     <body>

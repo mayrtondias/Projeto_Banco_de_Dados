cc1e348cb35adc5aa902f4286c177113af683237
Mayrton <mayrtondias@hotmail.com>
2014-03-18 22:18:30 -0300

    update

--- administrador/MenuAdministrador.php
+++ administrador/MenuAdministrador.php
@@ -4,21 +4,49 @@
     if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
         header('location: ../util/desconectado.php');
     }
+    //
 ?>
-    <div style="border-radius:1em; width:20%; font-size:25px; background:#3399FF;float: left">
+    <center>
+        
+        <h1 id="banner">GuaraGás - Satisfazendo pessoas em toda João Pessoa</h1>
         <center>
-            <a href="Home.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b> Home </b></a><br><hr>
-            <a href="HomeAdmin.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b> Gerenciar Administrador </b></a><br><hr>
-            <a href="HomeFunc.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b> Gerenciar Funcionário </b></a><br><hr>
-            <a href="HomeEstoque.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b> Estoque </b></a><br><hr>
-            <a href="HomeFinancas.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b> Finanças </b></a><br><hr>
-            <a href="../util/sair.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b> Sair </b></a><br>
-        </center>
-    </div>
-        <!-- logout!-->
-    <div style="border-radius:1em; width:25%; font-size:12px;background:#00BFFF; float: right; border-bottom: 5px">
-        <center>
-            <a href="../util/sair.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>Olá <?php echo $_SESSION['login'];?>, <i style="">Logout</i></b></a><br>
-        </center>
-    </div>
+        <div id="conteudo">
+            <ul id="menu">
+                <li><a href="Home.php"><b> Home </b></a><br>						
+                <li><b> Gerenciar Administrador </b>
+                    <ul>
+                        <li><a href="CadastraAdmin.php">Cadastrar Administrador</a></li>
+                        <li><a href="HomeAdminList.php">Listar Administrador</a></li>
+                        <li><a href="HomeAdminAtualizar.php">Atualizar Administrador</a></li>
+                        <li><a href="HomeAdminDelete.php">Excluir Administrador</a></li>								
+                    </ul>
+                <li><b> Gerenciar Funcionário </b>
+                    <ul>
+                        <li><a href="CadastraFunc.php">Cadastrar Funcionario</a></li>
+                        <li><a href="HomeFuncList.php">Listar Funcionario</a></li>
+                        <li><a href="HomeFuncAtualizar.php">Atualizar Funcionario</a></li>
+                        <li><a href="HomeFuncDelete.php">Excluir Funcionario</a></li>								
+                    </ul>
+                <li><b> Gerenciar Fornecedor </b>
+                    <ul>
+                        <li><a href="CadastraForne.php">Cadastrar Fornecedor</a></li>
+                        <li><a href="HomeForneList.php">Listar Fornecedor</a></li>
+                        <li><a href="HomeForneAtualizar.php">Atualizar Fornecedor</a></li>
+                        <li><a href="HomeForneDelete.php">Excluir Fornecedor</a></li>								
+                    </ul>
+                <li><b>Produtos</b>
+                    <ul>
+                        <li><a href="CadastraProduto.php">Cadastrar Produto</a></li>
+                        <li><a href="ProdutoList.php">Listar Produto</a></li>
+                    </ul>
+                <li><a href="../util/sair.php"><b> Sair </b></a><br></li>							
+            </ul>
+
+        </div> 	
+            </center>
+        <div id="dois">
+            Copyright © 2014 by Fellipe Mahon, Mayrton Dias e Vitor Soares.</br>
+            Todos os direitos reservados.
+       </div>
+    </center>
     <br>

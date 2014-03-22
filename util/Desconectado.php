d9e78bdde96fc0257e0aeb8d9fd4dff2460389d2
Mayrton <mayrtondias@hotmail.com>
2014-03-21 22:19:03 -0300

    update

--- util/Desconectado.php
+++ util/Desconectado.php
@@ -3,6 +3,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>GuaraGas</title>
+        <link rel="stylesheet" type="text/css" href="../estilos/Padrao.css" media="all"/>
     </head>
    
     <body>

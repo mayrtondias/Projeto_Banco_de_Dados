8cedf61de7f729c136222ce1dc98bf856e353d7d
Mayrton <mayrtondias@hotmail.com>
2014-03-26 20:28:49 -0300

    update

--- administrador/HomeAdminList.php
+++ administrador/HomeAdminList.php
@@ -34,57 +34,58 @@
                     echo "Problema na pesquisa<br>";
                 } else {
                     ?>
-                <br>
-               <center>
-                    <table border="1px" cellpadding="5px" cellspacing="0" ID="alter">
-                        <tr>
-                            <td></td>
-                            <td>Nome</td>
-                            <td>Login</td>
-                        </tr>
-                        <?php
-                        $contador=0;
-                        if( isset($_POST['selecionado'])==false){
-                            $qtdeResult=0;
-                        }else {
-                            if( isset($_POST['pagina'])==true){
-                                $qtdeResult=$_POST['pagina'];
-                            }
-                            if(($_POST['selecionado']==="anterior")||($qtdeResult>1)){
-                                --$qtdeResult;
-                            }else if($_POST['selecionado']==="proximo"){
+                    <br>
+                    <center>
+                        <table border="1px" cellpadding="5px" cellspacing="0" ID="alter">
+                            <tr>
+                                <td></td>
+                                <td>Nome</td>
+                                <td>Login</td>
+                            </tr>
+                            <?php
+                            $contador = 0;
+                            if (isset($_POST['selecionado']) == false) {
+                                $qtdeResult = 0;
+                            } else {
+                                if (isset($_POST['pagina']) == true) {
+                                    $qtdeResult = $_POST['pagina'];
+                                }
+                                if (($_POST['selecionado'] === "anterior") || ($qtdeResult > 1)) {
+                                    --$qtdeResult;
+                                } else if ($_POST['selecionado'] === "proximo") {
                                     ++$qtdeResult;
-                            }else $qtdeResult=0;
-                        }
-                        while ($registro = pg_fetch_array($resultado)) {
-
-                            if (($contador >= ($qtdeResult * 10)) && ($contador < ($qtdeResult * 10 + 10))) {
-                                ?>
-                                <tr>
-                                    <td><?php echo ($contador+1); ?></td>
-                                    <td><?php echo $registro['nome']; ?></td>
-                                    <td><?php echo $registro['login']; ?></td>
-                                </tr>
-                                <?php
+                                } else
+                                    $qtdeResult = 0;
                             }
-                            ++$contador;
-                        }
-                    }
-                    ?>
-                </table>
-                 </center>   
-                    <?php
-                    if ($qtdeResult > 0) {
-                        ?>
-                        <button type="submit" name="selecionado" value="anterior" >Anterior</button>
-                    <?php
-                    }
+                            while ($registro = pg_fetch_array($resultado)) {
 
-                    if ((($contador / 10) >= ($qtdeResult + 1)) && (($contador % 10) != 0)) {
+                                if (($contador >= ($qtdeResult * 10)) && ($contador < ($qtdeResult * 10 + 10))) {
+                                    ?>
+                                    <tr>
+                                        <td><?php echo ($contador + 1); ?></td>
+                                        <td><?php echo $registro['nome']; ?></td>
+                                        <td><?php echo $registro['login']; ?></td>
+                                    </tr>
+                                    <?php
+                                }
+                                ++$contador;
+                            }
+                        }
                         ?>
-                        <button type="submit" name="selecionado" value="proximo" >Próximo</button>
-                        <?php
-                    }
+                    </table>
+                </center>   
+                <?php
+                if ($qtdeResult > 0) {
+                    ?>
+                    <button type="submit" name="selecionado" value="anterior" >Anterior</button>
+                    <?php
+                }
+
+                if ((($contador / 10) >= ($qtdeResult + 1)) && (($contador % 10) != 0)) {
+                    ?>
+                    <button type="submit" name="selecionado" value="proximo" >Próximo</button>
+                    <?php
+                }
                 ?>
                 <input type="hidden" name="pagina" value="<?php echo $qtdeResult; ?>"><br>
             </form>

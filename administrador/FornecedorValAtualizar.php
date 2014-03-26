a2b9c1f60c23ff72fb9d07fc912cdcae2adaf781
Mayrton <mayrtondias@hotmail.com>
2014-03-26 20:28:15 -0300

    update

--- administrador/FornecedorValAtualizar.php
+++ administrador/FornecedorValAtualizar.php
@@ -4,91 +4,91 @@
     <head>
         <meta charset="UTF-8">
         <title>GuaraGas</title>
-    <link rel="stylesheet" type="text/css" href="../estilos/Padrao.css" media="all"/>
+        <link rel="stylesheet" type="text/css" href="../estilos/Padrao.css" media="all"/>
 
     </head>
-     
+
     <body>
         <?php
-            require 'MenuAdministrador.php';
-            
-            require '../banco/Banco.php';
-            
-            $banco=new banco();
-            session_start();
+        require 'MenuAdministrador.php';
 
-            if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
-                header('location: ../util/desconectado.php');
-            }
-          
-            $chave=$_POST['chave'];
-            
-            $tabela="fornecedor";
-            $pesquisa="*";
-           
-            $resultado=$banco->pesquisar($pesquisa, $tabela);
+        require '../banco/Banco.php';
 
-            if($resultado==NULL){
-                echo "Problema na pesquisa.<br>";
-            }else{
-                while($registro = pg_fetch_array($resultado)){
-                    if(($registro['nome']===$chave)){
-                        $_SESSION['atualNome']=$registro['nome'];
-                        $_SESSION['atualContato']=$registro['contato'];
-                        $_SESSION['atualRua']=$registro['rua'];
-                        $_SESSION['atualBairro']=$registro['bairro'];
-                        $_SESSION['atualNumero']=$registro['numero'];
-                        break;
-                    }
+        $banco = new banco();
+        session_start();
+
+        if (( isset($_SESSION['login']) == FALSE) || ( isset($_SESSION['senha']) == FALSE)) {
+            header('location: ../util/desconectado.php');
+        }
+
+        $chave = $_POST['chave'];
+
+        $tabela = "fornecedor";
+        $pesquisa = "*";
+
+        $resultado = $banco->pesquisar($pesquisa, $tabela);
+
+        if ($resultado == NULL) {
+            echo "Problema na pesquisa.<br>";
+        } else {
+            while ($registro = pg_fetch_array($resultado)) {
+                if (($registro['nome'] === $chave)) {
+                    $_SESSION['atualNome'] = $registro['nome'];
+                    $_SESSION['atualContato'] = $registro['contato'];
+                    $_SESSION['atualRua'] = $registro['rua'];
+                    $_SESSION['atualBairro'] = $registro['bairro'];
+                    $_SESSION['atualNumero'] = $registro['numero'];
+                    break;
                 }
             }
+        }
         ?>
-        
-        <center>
-            <div id="tres">
-                <form action="FornecedorValAtualizarExec.php" method="post">
-                    Informe os dos abaixo para Atualizar um Fornecedor <br>
-                    Nome:<br>
-                    <input type="text" name="nome" value="<?php echo $_SESSION['atualNome']; ?>"><br>
-                    Contato:<br>
-                    <input type="text" name="contato" value="<?php echo $_SESSION['atualContato']; ?>"><br>
-                    Rua:<br>
-                    <input type="text" name="rua" value="<?php echo $_SESSION['atualRua']; ?>"><br>
-                    Bairro:<br>
-                    <input type="text" name="bairro" value="<?php echo $_SESSION['atualBairro']; ?>"><br>
-                    Numero:<br>
-                    <input type="text" name="numero" value="<?php echo $_SESSION['atualNumero']; ?>"><br>
-                    <?php
-                        if(isset($_SESSION['erro'])==TRUE){
-                            $tipo=(int)$_SESSION['erro'];
-                            switch ($tipo){
-                                case 1:
-                                    $men="O campo nome não deve ficar em branco<br>nem superior a 30 caracteres<br>";
-                                    break;
-                                case 2:
-                                    $men="O campo contato não deve ficar em branco<br>deve ser nesse formato(xxx)xxxx-xxxx<br>";
-                                    break;
-                                case 3:
-                                    $men="O campo da rua não deve ficar em branco<br>nem superior a 50 caracteres.<br>";
-                                    break;
-                                case 4:
-                                    $men="O campo do bairro não deve ficar em branco<br>nem superior a 25 caracteres.<br>";
-                                    break;
-                                case 5:
-                                    $men="O campo do numero não deve ficar em branco<br>Digite um numero valido.<br>";
-                                    break;
-                                case 6:
-                                    $men="Endereço ja cadastrado no sistema<br>";
-                                    break;
-                                default :
-                                    $men="";
-                            }
-                            echo $men;
-                        }
-                    ?>
-                    <input type="submit" value="Atualizar">
-                </form>
-            </div>
-        </center>
-    </body>
+
+    <center>
+        <div id="tres">
+            <form action="FornecedorValAtualizarExec.php" method="post">
+                Informe os dos abaixo para Atualizar um Fornecedor <br>
+                Nome:<br>
+                <input type="text" name="nome" value="<?php echo $_SESSION['atualNome']; ?>"><br>
+                Contato:<br>
+                <input type="text" name="contato" value="<?php echo $_SESSION['atualContato']; ?>"><br>
+                Rua:<br>
+                <input type="text" name="rua" value="<?php echo $_SESSION['atualRua']; ?>"><br>
+                Bairro:<br>
+                <input type="text" name="bairro" value="<?php echo $_SESSION['atualBairro']; ?>"><br>
+                Numero:<br>
+                <input type="text" name="numero" value="<?php echo $_SESSION['atualNumero']; ?>"><br>
+                <?php
+                if (isset($_SESSION['erro']) == TRUE) {
+                    $tipo = (int) $_SESSION['erro'];
+                    switch ($tipo) {
+                        case 1:
+                            $men = "O campo nome não deve ficar em branco<br>nem superior a 30 caracteres<br>";
+                            break;
+                        case 2:
+                            $men = "O campo contato não deve ficar em branco<br>deve ser nesse formato(xxx)xxxx-xxxx<br>";
+                            break;
+                        case 3:
+                            $men = "O campo da rua não deve ficar em branco<br>nem superior a 50 caracteres.<br>";
+                            break;
+                        case 4:
+                            $men = "O campo do bairro não deve ficar em branco<br>nem superior a 25 caracteres.<br>";
+                            break;
+                        case 5:
+                            $men = "O campo do numero não deve ficar em branco<br>Digite um numero valido.<br>";
+                            break;
+                        case 6:
+                            $men = "Endereço ja cadastrado no sistema<br>";
+                            break;
+                        default :
+                            $men = "";
+                    }
+                    echo $men;
+                }
+                ?>
+                <input type="submit" value="Atualizar">
+            </form>
+        </div>
+    </center>
+</body>
 </html>

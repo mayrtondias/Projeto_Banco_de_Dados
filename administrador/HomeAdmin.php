d4d19ddd39c11e9b02b93e9def48adb69f5cc609
Mayrton <mayrtondias@hotmail.com>
2014-03-26 20:28:35 -0300

    update

--- administrador/HomeAdmin.php
+++ administrador/HomeAdmin.php
@@ -6,22 +6,22 @@
         <title>GuaraGas</title>
         <link rel="stylesheet" type="text/css" href="../estilos/Padrao.css" media="all"/>
     </head>
-     
+
     <body>
-        
+
         <?php
-            require 'MenuAdministrador.php';
+        require 'MenuAdministrador.php';
         ?>
-        
-        <center>
-            <div  id="tres">  
-                <center>
-                    <a href="CadastraAdmin.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cadastrar Administrador         </b></a><br><hr>
-                    <a href="HomeAdminList.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Listar Administradores         </b></a><br><hr>
-                    <a href="HomeAdminAtualizar.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Atualizar Conta        </b></a><br><hr>
-                    <a href="HomeAdminDelete.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Excluir Administrador         </b></a>
-                </center>
-            </div>
-        </center>
-    </body>
+
+    <center>
+        <div  id="tres">  
+            <center>
+                <a href="CadastraAdmin.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cadastrar Administrador         </b></a><br><hr>
+                <a href="HomeAdminList.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Listar Administradores         </b></a><br><hr>
+                <a href="HomeAdminAtualizar.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Atualizar Conta        </b></a><br><hr>
+                <a href="HomeAdminDelete.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Excluir Administrador         </b></a>
+            </center>
+        </div>
+    </center>
+</body>
 </html>

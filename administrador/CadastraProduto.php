9cf12a713eb3007b3c274135c2663d342506a600
Mayrton <mayrtondias@hotmail.com>
2014-03-26 20:28:10 -0300

    update

--- administrador/CadastraProduto.php
+++ administrador/CadastraProduto.php
@@ -6,53 +6,53 @@
         <title>GuaraGas</title>
         <link rel="stylesheet" type="text/css" href="../estilos/Padrao.css" media="all"/>
     </head>
-     
+
     <body>
-        
+
         <?php
-            require 'MenuAdministrador.php';
+        require 'MenuAdministrador.php';
         ?>
-        
-        <center>
-            <div id="tres">
-                <form action="ValidaProduto.php" method="post">
-                    Informe os dos abaixo para cadastrar um novo Produto<br>
-                    Nome:<br>
-                    <input type="text" name="nome"><br>
-                    Valor:<br>
-                    <input type="number" name="valor"><br>
-                    Codigo do produto:<br>
-                    <input type="number" name="codProduto"><br>
-                    Quantidade:<br>
-                    <input type="number" name="quantidade"><br>
-                        <?php
-                            if(isset($_SESSION['erro'])==TRUE){
-                                $tipo=(int)$_SESSION['erro'];
-                                switch ($tipo){
-                                    case 1:
-                                        $men="O campo nome não deve ficar em branco<br>nem superior a 50 caracteres<br>";
-                                        break;
-                                    case 2:
-                                        $men="O campo Valor não deve ficar em branco<br>nem ser um valor negativo<br>";
-                                        break;
-                                    case 3:
-                                        $men="O campo Codigo do Produto não deve ficar em branco<br>nem ser um valor negativo.<br>";
-                                        break;
-                                    case 4:
-                                        $men="O campo Quantidade não deve ficar em branco<br>nem ser um valor negativo<br>";
-                                        break;
-                                    case 5:
-                                        $men="Produto ja cadastrado no sistema<br>";
-                                        break;
-                                    default :
-                                        $men="";
-                                }
-                                echo $men;
-                            }
-                         ?>
-                    <input type="submit" value="Cadastrar">
-                </form>
-            </div>
-        </center>
-    </body>
+
+    <center>
+        <div id="tres">
+            <form action="ValidaProduto.php" method="post">
+                Informe os dos abaixo para cadastrar um novo Produto<br>
+                Nome:<br>
+                <input type="text" name="nome"><br>
+                Valor:<br>
+                <input type="number" name="valor"><br>
+                Codigo do produto:<br>
+                <input type="number" name="codProduto"><br>
+                Quantidade:<br>
+                <input type="number" name="quantidade"><br>
+                <?php
+                if (isset($_SESSION['erro']) == TRUE) {
+                    $tipo = (int) $_SESSION['erro'];
+                    switch ($tipo) {
+                        case 1:
+                            $men = "O campo nome não deve ficar em branco<br>nem superior a 50 caracteres<br>";
+                            break;
+                        case 2:
+                            $men = "O campo Valor não deve ficar em branco<br>nem ser um valor negativo<br>";
+                            break;
+                        case 3:
+                            $men = "O campo Codigo do Produto não deve ficar em branco<br>nem ser um valor negativo.<br>";
+                            break;
+                        case 4:
+                            $men = "O campo Quantidade não deve ficar em branco<br>nem ser um valor negativo<br>";
+                            break;
+                        case 5:
+                            $men = "Produto ja cadastrado no sistema<br>";
+                            break;
+                        default :
+                            $men = "";
+                    }
+                    echo $men;
+                }
+                ?>
+                <input type="submit" value="Cadastrar">
+            </form>
+        </div>
+    </center>
+</body>
 </html>

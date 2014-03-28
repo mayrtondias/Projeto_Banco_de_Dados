f98c310687f7a5b6395b28e31d270b8493d282d6
Mayrton <mayrtondias@hotmail.com>
2014-03-28 00:53:14 -0300

    update

--- administrador/HomeFunc.php
+++ administrador/HomeFunc.php
@@ -6,23 +6,23 @@
         <title>GuaraGas</title>
         <link rel="stylesheet" type="text/css" href="../estilos/Padrao.css" media="all"/>
     </head>
-     
+
     <body>
-        
+
         <?php
-            require 'MenuAdministrador.php';
+        require 'MenuAdministrador.php';
         ?>
-        
-        <center>
-            <div id="tres">  
-                <center>
-                    <a href="CadastraFunc.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cadastrar Funcionário         </b></a><br><hr>
-                    <a href="HomeFuncList.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Listar Funcionário         </b></a><br><hr>
-                    <a href="HomeFuncAtualizar.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Atualizar Conta        </b></a><br><hr>
-                    <a href="HomeFuncDelete.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Excluir Funcionário         </b></a><br>
-                    
-                </center>
-            </div>
-        </center>
-    </body>
+
+    <center>
+        <div id="tres">  
+            <center>
+                <a href="CadastraFunc.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Cadastrar Funcionário         </b></a><br><hr>
+                <a href="HomeFuncList.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Listar Funcionário         </b></a><br><hr>
+                <a href="HomeFuncAtualizar.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Atualizar Conta        </b></a><br><hr>
+                <a href="HomeFuncDelete.php" style="color:white; font-family:arial; text-decoration:none; padding:5px"><b>  Excluir Funcionário         </b></a><br>
+
+            </center>
+        </div>
+    </center>
+</body>
 </html>

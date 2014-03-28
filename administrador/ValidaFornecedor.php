f8f46dfd9b7cd2859ce40d53de26c33b814d9b8f
Mayrton <mayrtondias@hotmail.com>
2014-03-28 00:52:04 -0300

    update

--- administrador/ValidaFornecedor.php
+++ administrador/ValidaFornecedor.php
@@ -1,57 +1,56 @@
 <?php
-    
-    require '../banco/Banco.php';
-    
-    $banco=new banco();
-    session_start();
-    
-    if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
-        header('location: ../util/desconectado.php');
-    }
-            
-    $nome = $_POST['nome'];      
-    $contato = $_POST['contato'];
-    $rua = $_POST['rua'];      
-    $bairro = $_POST['bairro'];
-    $numero = $_POST['numero']; 
-    $fornecedorCadastrado="f";
-    $_SESSION['erro']="";
-    $tabela="fornecedor";
-    $pesquisa="*";
 
-    $resultado=$banco->pesquisar($pesquisa, $tabela);
+require '../banco/Banco.php';
 
-    if($resultado==NULL){
-        echo "Problema na pesquisa.<br>";
-    } else{
-          while($registro = pg_fetch_array($resultado)){
-              if($registro['nome']===$nome){
-                  $fornecedorCadastrado="t";
-              }
-          }
-     }
- 
-    if(($nome==="")||(strlen($nome)>30)){
-        $_SESSION['erro']="1";
-    } else if(($contato==="")||(strlen($contato)<8)||(strlen($contato)>14)){
-        $_SESSION['erro']="2";
-    } else if(($rua==="")||(strlen($rua)>50)){
-        $_SESSION['erro']="3";
-    } else if(($bairro==="")||(strlen($bairro)>25)){
-        $_SESSION['erro']="4";
-    } else if(!$numero){
-        $_SESSION['erro']="5";
-    } else if($clienteCadastrado==="t"){
-        $_SESSION['erro']="6";
-    } 
-    
-    if($_SESSION['erro'] === ""){
-        $banco->inserirFornecedor($nome, $contato, $rua, $bairro, $numero);
-        unset($_SESSION['erro']);
-        $_SESSION['mensagem']="1";
-        header('location: mensagem.php');
-    }else{
-        header('location: CadastraForne.php');
+$banco = new banco();
+session_start();
+
+if (( isset($_SESSION['login']) == FALSE) || ( isset($_SESSION['senha']) == FALSE)) {
+    header('location: ../util/desconectado.php');
+}
+
+$nome = $_POST['nome'];
+$contato = $_POST['contato'];
+$rua = $_POST['rua'];
+$bairro = $_POST['bairro'];
+$numero = $_POST['numero'];
+$fornecedorCadastrado = "f";
+$_SESSION['erro'] = "";
+$tabela = "fornecedor";
+$pesquisa = "*";
+
+$resultado = $banco->pesquisar($pesquisa, $tabela);
+
+if ($resultado == NULL) {
+    echo "Problema na pesquisa.<br>";
+} else {
+    while ($registro = pg_fetch_array($resultado)) {
+        if ($registro['nome'] === $nome) {
+            $fornecedorCadastrado = "t";
+        }
     }
-    
+}
+
+if (($nome === "") || (strlen($nome) > 30)) {
+    $_SESSION['erro'] = "1";
+} else if (($contato === "") || (strlen($contato) < 8) || (strlen($contato) > 14)) {
+    $_SESSION['erro'] = "2";
+} else if (($rua === "") || (strlen($rua) > 50)) {
+    $_SESSION['erro'] = "3";
+} else if (($bairro === "") || (strlen($bairro) > 25)) {
+    $_SESSION['erro'] = "4";
+} else if (!$numero) {
+    $_SESSION['erro'] = "5";
+} else if ($clienteCadastrado === "t") {
+    $_SESSION['erro'] = "6";
+}
+
+if ($_SESSION['erro'] === "") {
+    $banco->inserirFornecedor($nome, $contato, $rua, $bairro, $numero);
+    unset($_SESSION['erro']);
+    $_SESSION['mensagem'] = "1";
+    header('location: mensagem.php');
+} else {
+    header('location: CadastraForne.php');
+}
 ?>

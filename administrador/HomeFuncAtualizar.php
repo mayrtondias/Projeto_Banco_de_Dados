4dd11059899be4c18211ec1aa88f5c3136a1140b
Mayrton <mayrtondias@hotmail.com>
2014-03-28 00:53:06 -0300

    update

--- administrador/HomeFuncAtualizar.php
+++ administrador/HomeFuncAtualizar.php
@@ -8,60 +8,60 @@
     </head>
 
     <body>
-        
+
         <?php
-            require 'MenuAdministrador.php';
+        require 'MenuAdministrador.php';
         ?>
-        <center>
-           <div id="tres">
-                <form method="POST" action="HomeFuncValAtualizar.php" >
-                    <?php
+    <center>
+        <div id="tres">
+            <form method="POST" action="HomeFuncValAtualizar.php" >
+                <?php
+                require '../banco/Banco.php';
 
-                    require '../banco/Banco.php';
+                $banco = new Banco();
+                session_start();
 
-                    $banco=new Banco();
-                    session_start();
+                if (( isset($_SESSION['login']) == FALSE) || ( isset($_SESSION['senha']) == FALSE)) {
+                    header('location: ../util/desconectado.php');
+                }
 
-                    if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
-                        header('location: ../util/desconectado.php');
-                    }
-                    
-                    $tabela="funcionario";
-                    $pesquisa="*";
-                       
-                    $resultado=$banco->pesquisar($pesquisa, $tabela);
+                $tabela = "funcionario";
+                $pesquisa = "*";
 
-                    if($resultado==NULL){
-                        echo "Problema na pesquisa<br>";
-                    } else{
-                        ?>
-                <table>
-                    <tr>
-                        <td></td>
-                        <td>Nome</td>
-                        <td>Login</td>
-                        <td>Identidade</td>
-                        <td>CPF</td>
-                        <td>Telefone</td>
-                        <td>Cargo</td>
-                        <td>Atualizar</td>
-                    </tr>
+                $resultado = $banco->pesquisar($pesquisa, $tabela);
+
+                if ($resultado == NULL) {
+                    echo "Problema na pesquisa<br>";
+                } else {
+                    ?>
+                    <table>
+                        <tr>
+                            <td></td>
+                            <td>Nome</td>
+                            <td>Login</td>
+                            <td>Identidade</td>
+                            <td>CPF</td>
+                            <td>Telefone</td>
+                            <td>Cargo</td>
+                            <td>Atualizar</td>
+                        </tr>
                         <?php
-                        $contador=0;
-                        if( isset($_POST['selecionado'])==false){
-                            $qtdeResult=0;
-                        }else if(($_POST['selecionado']==="anterior")||($qtdeResult>1)){
-                                --$qtdeResult;
-                        }else if($_POST['selecionado']==="proximo"){
-                                ++$qtdeResult;
-                        }else $qtdeResult=0;
+                        $contador = 0;
+                        if (isset($_POST['selecionado']) == false) {
+                            $qtdeResult = 0;
+                        } else if (($_POST['selecionado'] === "anterior") || ($qtdeResult > 1)) {
+                            --$qtdeResult;
+                        } else if ($_POST['selecionado'] === "proximo") {
+                            ++$qtdeResult;
+                        } else
+                            $qtdeResult = 0;
 
-                        while($registro = pg_fetch_array($resultado)){
-                              
-                            if(($contador>=($qtdeResult*10))&&($contador<($qtdeResult*10+10))){
-                                  ?>
-                                  <tr>
-                                    <td><?php echo ($contador+1); ?></td>
+                        while ($registro = pg_fetch_array($resultado)) {
+
+                            if (($contador >= ($qtdeResult * 10)) && ($contador < ($qtdeResult * 10 + 10))) {
+                                ?>
+                                <tr>
+                                    <td><?php echo ($contador + 1); ?></td>
                                     <td><?php echo $registro['nome']; ?></td>
                                     <td><?php echo $registro['login']; ?></td>
                                     <td><?php echo $registro['identidade']; ?></td>
@@ -69,34 +69,31 @@
                                     <td><?php echo $registro['telefone']; ?></td>
                                     <td><?php echo $registro['cargo']; ?></td>
                                     <td><button type="submit" name="chave" value="<?php echo $registro['cpf']; ?>" >OK</button></td>
-                                  </tr>
+                                </tr>
                                 <?php
-                                
-                              }
-                              ++$contador;
-                          }
-                      }
-            ?>
+                            }
+                            ++$contador;
+                        }
+                    }
+                    ?>
                 </table>
             </form>
-                <form method="POST" action="HomeFuncAtualizar.php" >
+            <form method="POST" action="HomeFuncAtualizar.php" >
+                <?php
+                if ($qtdeResult > 0) {
+                    ?>
+                    <button type="submit" name="selecionado" value="anterior" >Anterior</button>
                     <?php
-                    
-                if($qtdeResult>0){
-                              ?>
-                              <button type="submit" name="selecionado" value="anterior" >Anterior</button>
-                              <?php
-                              
-                          }
-                          
-                          if((($contador/10)>=($qtdeResult+1))&&(($contador%10)!=0)){
-                              ?>
-                              <button type="submit" name="selecionado" value="proximo" >Próximo</button>
-                              <?php
-                          }
-                 ?>
-                </form>
-            </div>
-        </center>
-    </body>
+                }
+
+                if ((($contador / 10) >= ($qtdeResult + 1)) && (($contador % 10) != 0)) {
+                    ?>
+                    <button type="submit" name="selecionado" value="proximo" >Próximo</button>
+                    <?php
+                }
+                ?>
+            </form>
+        </div>
+    </center>
+</body>
 </html>

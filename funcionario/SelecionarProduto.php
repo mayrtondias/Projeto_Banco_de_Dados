137fb250774bd38f6b9827d926cd4f4c369b5bf3
Mayrton <mayrtondias@hotmail.com>
2014-03-29 20:12:29 -0300

    update

--- funcionario/SelecionarProduto.php
+++ funcionario/SelecionarProduto.php
@@ -19,49 +19,49 @@
                         <td>Selecionar</td>
                     </tr>
 
-                <?php
+                    <?php
                     require '../banco/Banco.php';
-                    
-                    $banco=new banco();
+
+                    $banco = new banco();
                     session_start();
 
-                    if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
+                    if (( isset($_SESSION['login']) == FALSE) || ( isset($_SESSION['senha']) == FALSE)) {
                         header('location: ../util/desconectado.php');
                     }
-                    
-                    
-                    $chave=explode("§", $_POST['selecionado']);
-                    $rua=$chave[0];
-                    $bairro=$chave[1];
-                    $numero=$chave[2];
 
-                    $_SESSION['rua']=$rua;
-                    $_SESSION['bairro']=$bairro;
-                    $_SESSION['numero']=$numero;
 
-                    $tabela="produto";
-                    $pesquisa="*";
+                    $chave = explode("§", $_POST['selecionado']);
+                    $rua = $chave[0];
+                    $bairro = $chave[1];
+                    $numero = $chave[2];
 
-                    $resultado=$banco->pesquisar($pesquisa, $tabela);
+                    $_SESSION['rua'] = $rua;
+                    $_SESSION['bairro'] = $bairro;
+                    $_SESSION['numero'] = $numero;
 
-                    if($resultado==NULL){
+                    $tabela = "produto";
+                    $pesquisa = "*";
+
+                    $resultado = $banco->pesquisar($pesquisa, $tabela);
+
+                    if ($resultado == NULL) {
                         echo "Problema na pesquisa.<br>";
-                    } else{
-                          while($registro = pg_fetch_array($resultado)){
-                              ?>
-                                <tr>
-                                  <td><?php echo $registro['nome']; ?></td>
-                                  <td><?php echo $registro['valor']; ?></td>
-                                  <td><?php echo $registro['codproduto']; ?></td>
-                                  <td><?php echo $registro['quantidade']; ?></td>
-                                  
-                                  <td><input id="teste" type="text" name="teste[]"><br></td>
-                                  <td><button type="submit" name="codigo" value="<?php echo $registro['codproduto']; ?>" >OK</button></td>
-                                </tr>
-                              <?php
-                              }
-                          }
-                ?>
+                    } else {
+                        while ($registro = pg_fetch_array($resultado)) {
+                            ?>
+                            <tr>
+                                <td><?php echo $registro['nome']; ?></td>
+                                <td><?php echo $registro['valor']; ?></td>
+                                <td><?php echo $registro['codproduto']; ?></td>
+                                <td><?php echo $registro['quantidade']; ?></td>
+
+                                <td><input id="teste" type="text" name="teste[]"><br></td>
+                                <td><button type="submit" name="codigo" value="<?php echo $registro['codproduto']; ?>" >OK</button></td>
+                            </tr>
+                            <?php
+                        }
+                    }
+                    ?>
                 </table>
             </form>
         </div>

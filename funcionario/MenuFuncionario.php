87111d303e7f3a1440e76030012add33bfc75f6a
Mayrton <mayrtondias@hotmail.com>
2014-03-29 20:12:24 -0300

    update

--- funcionario/MenuFuncionario.php
+++ funcionario/MenuFuncionario.php
@@ -1,38 +1,38 @@
 <?php
-    session_start();
+session_start();
 
-    if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
-        header('location: ../util/desconectado.php');
-    }
-    //<h1 id="banner">GuaraGás - Satisfazendo pessoas em toda João Pessoa</h1>
+if (( isset($_SESSION['login']) == FALSE) || ( isset($_SESSION['senha']) == FALSE)) {
+    header('location: ../util/desconectado.php');
+}
+//<h1 id="banner">GuaraGás - Satisfazendo pessoas em toda João Pessoa</h1>
 ?>
-    <center>
-        
-        <h1 id="banner">GuaraGás - Satisfazendo pessoas em toda João Pessoa</h1>
+<center>
 
-        <div id="conteudo">
-            <ul id="menu">
-                <li><a href="Home.php"><b> Home </b></a><br>						
-                <li><b> Realizar Vendas </b>
-                    <ul>
-                        <li><a href="Vendas.php">Realizar Venda</a></li>								
-                    </ul>
-                <li><b> Gerenciar Clientes </b>
-                    <ul>
-                        <li><a href="ClienteCadastrar.php">Cadastrar Cliente</a></li>
-                        <li><a href="ClienteList.php">Listar Cliente</a></li>
-                        <li><a href="ClienteAtualizar.php">Atualizar Cliente</a></li>
-                        <li><a href="ClienteDelete.php">Excluir Cliente</a></li>								
-                    </ul>
-                
-                <li><a href="../util/sair.php"><b> Sair </b></a><br></li>							
-            </ul>
+    <h1 id="banner">GuaraGás - Satisfazendo pessoas em toda João Pessoa</h1>
 
-        </div> 	
+    <div id="conteudo">
+        <ul id="menu">
+            <li><a href="Home.php"><b> Home </b></a><br>						
+            <li><b> Realizar Vendas </b>
+                <ul>
+                    <li><a href="Vendas.php">Realizar Venda</a></li>								
+                </ul>
+            <li><b> Gerenciar Clientes </b>
+                <ul>
+                    <li><a href="ClienteCadastrar.php">Cadastrar Cliente</a></li>
+                    <li><a href="ClienteList.php">Listar Cliente</a></li>
+                    <li><a href="ClienteAtualizar.php">Atualizar Cliente</a></li>
+                    <li><a href="ClienteDelete.php">Excluir Cliente</a></li>								
+                </ul>
 
-        <div id="dois">
-            Copyright © 2014 by Fellipe Mahon, Mayrton Dias e Vitor Soares.</br>
-            Todos os direitos reservados.
-        </div>
-    </center>
-    <br>
+            <li><a href="../util/sair.php"><b> Sair </b></a><br></li>							
+        </ul>
+
+    </div> 	
+
+    <div id="dois">
+        Copyright © 2014 by Fellipe Mahon, Mayrton Dias e Vitor Soares.</br>
+        Todos os direitos reservados.
+    </div>
+</center>
+<br>

3c0e64928e6e42e4e7dcbf3f586cf792e4b4ff53
Mayrton <mayrtondias@hotmail.com>
2014-03-29 20:10:19 -0300

    update

--- funcionario/Home.php
+++ funcionario/Home.php
@@ -6,10 +6,10 @@
         <title>GuaraGas </title>
         <link rel="stylesheet" type="text/css" href="../estilos/Padrao.css" media="all"/>
     </head>
-     
+
     <body>
         <?php
-            require 'MenuFuncionario.php';
+        require 'MenuFuncionario.php';
         ?>
     </body>
 </html>

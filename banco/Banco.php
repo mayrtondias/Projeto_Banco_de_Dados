d1077e893d464570cb4bcd92ab0fb3c6244d45c3
Mayrton <mayrtondias@hotmail.com>
2014-03-29 20:08:16 -0300

    update

--- banco/Banco.php
+++ banco/Banco.php
@@ -6,139 +6,133 @@
  */
 
 class Banco {
-    
+
     //construtor da Classe
     public function __construct() {
-        $conetar=$this->conectarBanco();
+        $conetar = $this->conectarBanco();
     }
-    
+
     //metodo para connectar o Banco de Dados
     private function conectarBanco() {
         $host = "localhost"; //host
         $db = "guaragas";   //nome do banco de dados
         $user = "postgres"; //usuario do banco de dados
         $passwd = "12345678"; //senha do banco de dados
-
         //Conectando ao banco de dados guaragas
         $conn = pg_connect("host=$host dbname=$db user=$user password=$passwd");
-    
+
         return $conn;
     }
-    
-    
-    public function inserirProduto($nome, $valor, $codigo,$quantidade){
-        
-        $conexao=$this->conectarBanco();
-        
-       //Query para inserir 1 registro na tabela
+
+    public function inserirProduto($nome, $valor, $codigo, $quantidade) {
+
+        $conexao = $this->conectarBanco();
+
+        //Query para inserir 1 registro na tabela
         $reg_query = "INSERT INTO produto  VALUES ('$nome',$valor,$codigo,$quantidade);";
-        
+
         //Executando query para inserir o registro na tabela produto
         $res = pg_query($conexao, $reg_query);
         return $res;
     }
-    
-    public function inserirAdministrador($nome, $login, $senha){
-        
-        $conexao=$this->conectarBanco();
-        
-       //Query para inserir 1 registro na tabela administrador
+
+    public function inserirAdministrador($nome, $login, $senha) {
+
+        $conexao = $this->conectarBanco();
+
+        //Query para inserir 1 registro na tabela administrador
         $reg_query = "INSERT INTO administrador  VALUES ('$nome','$login','$senha');";
-        
+
         //Executando query para inserir o registro na tabela
         $res = pg_query($conexao, $reg_query);
         return $res;
     }
-    
-    public function inserirCliente($nome, $contato, $rua, $bairro, $numero){
-        
-        $conexao=$this->conectarBanco();
-        
-       //Query para inserir 1 registro na tabela administrador
+
+    public function inserirCliente($nome, $contato, $rua, $bairro, $numero) {
+
+        $conexao = $this->conectarBanco();
+
+        //Query para inserir 1 registro na tabela administrador
         $reg_query = "INSERT INTO cliente  VALUES ('$nome','$contato','$rua','$bairro',$numero);";
-        
+
         //Executando query para inserir o registro na tabela
         $res = pg_query($conexao, $reg_query);
         return $res;
     }
-    
-    public function inserirFornecedor($nome, $contato, $rua, $bairro, $numero){
-        
-        $conexao=$this->conectarBanco();
-        
-       //Query para inserir 1 registro na tabela administrador
+
+    public function inserirFornecedor($nome, $contato, $rua, $bairro, $numero) {
+
+        $conexao = $this->conectarBanco();
+
+        //Query para inserir 1 registro na tabela administrador
         $reg_query = "INSERT INTO fornecedor  VALUES ('$nome','$contato','$rua','$bairro',$numero);";
-        
+
         //Executando query para inserir o registro na tabela
         $res = pg_query($conexao, $reg_query);
         return $res;
     }
-    
-    public function inserirVenda($data, $hora, $valor, $quantidade, $codigoProduto, $status, $rua, $bairro, $numero){
-        
-        $conexao=$this->conectarBanco();
-        
-       //Query para inserir 1 registro na tabela administrador
+
+    public function inserirVenda($data, $hora, $valor, $quantidade, $codigoProduto, $status, $rua, $bairro, $numero) {
+
+        $conexao = $this->conectarBanco();
+
+        //Query para inserir 1 registro na tabela administrador
         $reg_query = "INSERT INTO venda  VALUES ('$data','$hora',$valor,$quantidade,$codigoProduto,'$status','$rua','$bairro',$numero);";
-        
+
         //Executando query para inserir o nregistro na tabela
         $res = pg_query($conexao, $reg_query);
         return $res;
     }
-    
-    public function inserirFuncionario($cpf, $nome, $identidade, $login, $senha, $salario, $telefone, $cargo, $datacon, $datadem){
-        
-        $conexao=$this->conectarBanco();
-        
-       //Query para inserir 1 registro na tabela administrador
+
+    public function inserirFuncionario($cpf, $nome, $identidade, $login, $senha, $salario, $telefone, $cargo, $datacon, $datadem) {
+
+        $conexao = $this->conectarBanco();
+
+        //Query para inserir 1 registro na tabela administrador
         $reg_query = "INSERT INTO funcionario  VALUES ('$cpf','$nome','$identidade','$login','$senha',$salario,'$telefone','$cargo','$datacon','$datadem');";
-        
+
         //Executando query para inserir o registro na tabela
         $res = pg_query($conexao, $reg_query);
         return $res;
     }
-        
-    public function pesquisar($pesquisa, $tabela){
-        
-        $conexao=$this->conectarBanco();
-        
+
+    public function pesquisar($pesquisa, $tabela) {
+
+        $conexao = $this->conectarBanco();
+
         //Query para listar registros da tabela
         $show_query = "SELECT $pesquisa FROM $tabela";
-        
+
         //Executando query para listar os registros da tabela
         $res = pg_query($conexao, $show_query);
-        
+
         return $res;
     }
-    
-    
-    
-    public function deletar($tabela, $clausuraWere){
-        
-        $conexao=$this->conectarBanco();
-        
+
+    public function deletar($tabela, $clausuraWere) {
+
+        $conexao = $this->conectarBanco();
+
         //Query para listar registros da tabela
         $show_query = "DELETE FROM $tabela WHERE $clausuraWere;";
-        
+
         //Executando query para listar os registros da tabela
         $res = pg_query($conexao, $show_query);
-        
+
         return $res;
     }
-    
-    
-    public function update($tabela, $clausuraSET, $clausuraWere){
-        
-        $conexao=$this->conectarBanco();
-        
+
+    public function update($tabela, $clausuraSET, $clausuraWere) {
+
+        $conexao = $this->conectarBanco();
+
         //Query para listar registros da tabela
         $show_query = "UPDATE $tabela SET $clausuraSET WHERE $clausuraWere";
-        
+
         //Executando query para listar os registros da tabela
         $res = pg_query($conexao, $show_query);
-        
+
         return $res;
     }
-    
-}
 
+}

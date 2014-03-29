5957af28c484bc77773956a6b7a6b4ce3a487817
Mayrton <mayrtondias@hotmail.com>
2014-03-29 20:10:10 -0300

    update

--- funcionario/ClienteValDelete.php
+++ funcionario/ClienteValDelete.php
@@ -1,28 +1,28 @@
 <?php
-    require '../banco/Banco.php';
 
-    $banco=new banco();
-    session_start();
+require '../banco/Banco.php';
 
-    if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
-        header('location: ../util/desconectado.php');
-    }
+$banco = new banco();
+session_start();
 
-    $chave=explode("§", $_POST['chave']);
-                    $rua=$chave[0];
-                    $bairro=$chave[1];
-                    $numero=$chave[2];
-    
-    $tabela="cliente";
-    $clausuraWere="rua = '$rua' AND bairro = '$bairro' AND numero = '$numero'";
+if (( isset($_SESSION['login']) == FALSE) || ( isset($_SESSION['senha']) == FALSE)) {
+    header('location: ../util/desconectado.php');
+}
 
-    $resultado=$banco->deletar($tabela, $clausuraWere);
-    
-    if($resultado==NULL){
-        echo "Problema na exclusão.<br>";
-    }else{
-        $_SESSION['mensagem']="2";
-        header('location: mensagem.php');
-        
-    }
-    ?>
+$chave = explode("§", $_POST['chave']);
+$rua = $chave[0];
+$bairro = $chave[1];
+$numero = $chave[2];
+
+$tabela = "cliente";
+$clausuraWere = "rua = '$rua' AND bairro = '$bairro' AND numero = '$numero'";
+
+$resultado = $banco->deletar($tabela, $clausuraWere);
+
+if ($resultado == NULL) {
+    echo "Problema na exclusão.<br>";
+} else {
+    $_SESSION['mensagem'] = "2";
+    header('location: mensagem.php');
+}
+?>

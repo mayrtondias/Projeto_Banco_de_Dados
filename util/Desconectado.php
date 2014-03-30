2cc49cb728e524bda3ccabf320652da295e2f0d1
Mayrton <mayrtondias@hotmail.com>
2014-03-30 09:56:27 -0300

    update

--- util/Desconectado.php
+++ util/Desconectado.php
@@ -5,21 +5,21 @@
         <title>GuaraGas</title>
         <link rel="stylesheet" type="text/css" href="../estilos/Padrao.css" media="all"/>
     </head>
-   
+
     <body>
-        <center>
-            <div  style="border-radius:1em; width:50%; font-size:19px; background:#99CCFF;color:white" >
-                <center>
-                    <h1>Sua sessão expirou! </h1>
-                </center>
-            </div>
-            <br>
-            <div  style="border-radius:1em; width:30%; font-size:19px;background:#99CCFF;color:white">
-                <center>
-                    <h3><a href="../index.php" style="color:white;text-decoration:none">LogIn</a></h3>
-                </center>
-            </div>
-        </center>    
-    </body>
+    <center>
+        <div  style="border-radius:1em; width:50%; font-size:19px; background:#99CCFF;color:white" >
+            <center>
+                <h1>Sua sessão expirou! </h1>
+            </center>
+        </div>
+        <br>
+        <div  style="border-radius:1em; width:30%; font-size:19px;background:#99CCFF;color:white">
+            <center>
+                <h3><a href="../index.php" style="color:white;text-decoration:none">LogIn</a></h3>
+            </center>
+        </div>
+    </center>    
+</body>
 </html>
 

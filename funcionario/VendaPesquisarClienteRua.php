cd07c22720d06d3394996c85f5cd9c72c3abff61
Mayrton <mayrtondias@hotmail.com>
2014-03-30 09:56:02 -0300

    update

--- funcionario/VendaPesquisarClienteRua.php
+++ funcionario/VendaPesquisarClienteRua.php
@@ -19,51 +19,48 @@
                         <td>Contato</td>
                         <td>Selecionar</td>
                     </tr>
-                <?php
-
+                    <?php
                     require '../banco/Banco.php';
 
-                    $banco=new banco();
+                    $banco = new banco();
                     session_start();
 
-                    if( ( isset($_SESSION['login']) == FALSE)||( isset($_SESSION['senha']) == FALSE) ){
+                    if (( isset($_SESSION['login']) == FALSE) || ( isset($_SESSION['senha']) == FALSE)) {
                         header('location: ../util/desconectado.php');
                     }
 
-                    $rua = $_POST['pesquisarRua'];      
-                    $_SESSION['erro']="";
-                    $tabela="cliente";
-                    $pesquisa="*";
+                    $rua = $_POST['pesquisarRua'];
+                    $_SESSION['erro'] = "";
+                    $tabela = "cliente";
+                    $pesquisa = "*";
 
-                    if(($nome==="")||(strlen($nome)>50)){
-                        $_SESSION['erro']="2";
+                    if (($nome === "") || (strlen($nome) > 50)) {
+                        $_SESSION['erro'] = "2";
                         header('location: Vendas.php');
                     }
 
-                    $resultado=$banco->pesquisar($pesquisa, $tabela);
+                    $resultado = $banco->pesquisar($pesquisa, $tabela);
 
-                    if($resultado==NULL){
+                    if ($resultado == NULL) {
                         echo "Problema na pesquisa<br>";
-                    } else{
-                          while($registro = pg_fetch_array($resultado)){
+                    } else {
+                        while ($registro = pg_fetch_array($resultado)) {
 
-                              if($registro['rua']===$rua){
-                                  ?>
-                                  <tr>
+                            if ($registro['rua'] === $rua) {
+                                ?>
+                                <tr>
                                     <td><?php echo $registro['nome']; ?></td>
                                     <td><?php echo $registro['rua']; ?></td>
                                     <td><?php echo $registro['bairro']; ?></td>
                                     <td><?php echo $registro['numero']; ?></td>
                                     <td><?php echo $registro['contato']; ?></td>
-                                    <td><button type="submit" name="selecionado" value="<?php echo $registro['rua']."§".$registro['bairro']."§".$registro['numero']; ?>" >OK</button></td>
+                                    <td><button type="submit" name="selecionado" value="<?php echo $registro['rua'] . "§" . $registro['bairro'] . "§" . $registro['numero']; ?>" >OK</button></td>
                                 </tr>
                                 <?php
-                              }
-                          }
-                     }
-
-
-            ?>
+                            }
+                        }
+                    }
+                    ?>
                 </table>
             </form>
         </div>

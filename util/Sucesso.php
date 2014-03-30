b269251b3f367d74c97e8b157c4c70300a47b890
Mayrton <mayrtondias@hotmail.com>
2014-03-30 09:56:36 -0300

    update

--- util/Sucesso.php
+++ util/Sucesso.php
@@ -6,10 +6,10 @@
         <title>Guaragas</title>
         <link rel="stylesheet" type="text/css" href="../estilos/Padrao.css" media="all"/>
     </head>
-    
+
     <body>
-        <center>
-            <a href="../programador/ProgramadorHome.php" style="border-radius:1em;color:#FFFFFF; font-family:arial; text-decoration:none; padding:5px; background:#1C86EE"><b>Operação realizada com Sucesso!</b></a>
-        </center>
-    </body>
+    <center>
+        <a href="../programador/ProgramadorHome.php" style="border-radius:1em;color:#FFFFFF; font-family:arial; text-decoration:none; padding:5px; background:#1C86EE"><b>Operação realizada com Sucesso!</b></a>
+    </center>
+</body>
 </html>

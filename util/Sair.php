faa55251977e6bb3af2ec9fb8a78a96bf58cfc71
Mayrton <mayrtondias@hotmail.com>
2014-03-30 09:56:32 -0300

    update

--- util/Sair.php
+++ util/Sair.php
@@ -1,15 +1,15 @@
 <?php
-    session_start();
+session_start();
 
-    if(isset($_SESSION['nome']) == TRUE){
-        unset($_SESSION['nome']);
-    }
-   
-    if(isset($_SESSION['email']) == TRUE) {
-        unset($_SESSION['email']);
-    }
-   
-    session_destroy();
+if (isset($_SESSION['nome']) == TRUE) {
+    unset($_SESSION['nome']);
+}
+
+if (isset($_SESSION['email']) == TRUE) {
+    unset($_SESSION['email']);
+}
+
+session_destroy();
 ?>
 <!DOCTYPE html>
 
@@ -20,9 +20,9 @@
         <link rel="stylesheet" type="text/css" href="../estilos/Padrao.css" media="all"/>
     </head>
     <body>
-        <center>
-            <p style="font-family: arial; font-size: 16px; color:#1C86EE"><b>Usuario desconectado!</b></p>
-            <a href="../index.php" style="border-radius:1em;color:#FFFFFF; font-family:arial; text-decoration:none; padding:5px;background:#1C86EE"><b>Voltar ao Sistema </b></a>
-        </center>
-    </body>
+    <center>
+        <p style="font-family: arial; font-size: 16px; color:#1C86EE"><b>Usuario desconectado!</b></p>
+        <a href="../index.php" style="border-radius:1em;color:#FFFFFF; font-family:arial; text-decoration:none; padding:5px;background:#1C86EE"><b>Voltar ao Sistema </b></a>
+    </center>
+</body>
 </html>

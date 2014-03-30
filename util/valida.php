bedc14be22409ea7e5513c91b4ba6156c84717a7
Mayrton <mayrtondias@hotmail.com>
2014-03-30 09:57:09 -0300

    update

--- util/valida.php
+++ util/valida.php
@@ -2,61 +2,60 @@
     <head>
         <title>GuaraGas</title>
     </head>
-   
+
     <body>
         <?php
-        
-            require '../banco/Banco.php';
-            $banco=new banco();
-            session_start();
-            
-            $login = $_POST['login'];      
-            $senha = $_POST['senha'];
-            
-            $_SESSION['login']="";
-            $_SESSION['senha']="";
-            
-            $tabela="administrador";
-            $pesquisa="*";
-            
-            $resultado=$banco->pesquisar($pesquisa, $tabela);
+        require '../banco/Banco.php';
+        $banco = new banco();
+        session_start();
 
-            if($resultado==NULL){
-                echo "Problema com a pesquisa! ".pg_errormessage();
-            } else{
-                while($registro = pg_fetch_array($resultado)){
-                    
-                    if(($registro['login']===$login)&&($registro['senha']===$senha)){
-                        $_SESSION['login']=$registro['login'];
-                        $_SESSION['senha']=$registro['senha'];
-                        
-                        header('location: ../administrador/Home.php');
-                    }
+        $login = $_POST['login'];
+        $senha = $_POST['senha'];
+
+        $_SESSION['login'] = "";
+        $_SESSION['senha'] = "";
+
+        $tabela = "administrador";
+        $pesquisa = "*";
+
+        $resultado = $banco->pesquisar($pesquisa, $tabela);
+
+        if ($resultado == NULL) {
+            echo "Problema com a pesquisa! " . pg_errormessage();
+        } else {
+            while ($registro = pg_fetch_array($resultado)) {
+
+                if (($registro['login'] === $login) && ($registro['senha'] === $senha)) {
+                    $_SESSION['login'] = $registro['login'];
+                    $_SESSION['senha'] = $registro['senha'];
+
+                    header('location: ../administrador/Home.php');
                 }
             }
-            
-            $tabela="funcionario";            
-            $resultado=$banco->pesquisar($pesquisa, $tabela);
+        }
 
-            if($resultado==NULL){
-                echo "Problema com a pesquisa! ".pg_errormessage();
-            } else{
-                while($registro = pg_fetch_array($resultado)){
-                    
-                    if(($registro['login']===$login)&&($registro['senha']===$senha)){
-                        $_SESSION['login']=$registro['login'];
-                        $_SESSION['senha']=$registro['senha'];
-                        header('location: ../funcionario/Home.php');
-                    }
+        $tabela = "funcionario";
+        $resultado = $banco->pesquisar($pesquisa, $tabela);
+
+        if ($resultado == NULL) {
+            echo "Problema com a pesquisa! " . pg_errormessage();
+        } else {
+            while ($registro = pg_fetch_array($resultado)) {
+
+                if (($registro['login'] === $login) && ($registro['senha'] === $senha)) {
+                    $_SESSION['login'] = $registro['login'];
+                    $_SESSION['senha'] = $registro['senha'];
+                    header('location: ../funcionario/Home.php');
                 }
             }
-            
-            if( ($_SESSION['login']==="")&&($_SESSION['senha']==="")){
-                $_SESSION['login']="erro";
-                $_SESSION['senha']="erro";
-                header('location: indexErro.php');
-            } 
+        }
+
+        if (($_SESSION['login'] === "") && ($_SESSION['senha'] === "")) {
+            $_SESSION['login'] = "erro";
+            $_SESSION['senha'] = "erro";
+            header('location: indexErro.php');
+        }
         ?>
-        
+
     </body>
 </html>

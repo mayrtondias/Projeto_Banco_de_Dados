8f4e9b5c817d206d65f6aeafcfcb8ac25d35ecee
Mayrton <mayrtondias@hotmail.com>
2014-03-30 09:56:46 -0300

    update

--- util/indexErro.php
+++ util/indexErro.php
@@ -6,29 +6,29 @@
         <title>Guaragas</title>
         <link rel="stylesheet" type="text/css" href="../estilos/Padrao.css" media="all"/>
     </head>
-     
+
     <body>
         <?php
-            require 'MenuFuncionario.php';
+        require 'MenuFuncionario.php';
         ?>
-        <center>
-            <div style="border-radius:1em; width:20%; font-size:25px; background:#3399FF; margin: 15%">
-                <form action="valida.php" method="post">
-                    <label style="color:white; font-family:arial; text-decoration:none; padding:5px">Login:</label><br>
-                    <input type="text" name="login"><br>
-                    <label style="color:white; font-family:arial; text-decoration:none; padding:5px">Senha:</label><br>
-                    <input type="password" name="senha"><br>
-                    
-                    <label style="color:red; font-family:arial; text-decoration:none; padding:5px; font-size: 14px">
-                        
-                        <b>Atenção: Não foi possível validar seu acesso!
+    <center>
+        <div style="border-radius:1em; width:20%; font-size:25px; background:#3399FF; margin: 15%">
+            <form action="valida.php" method="post">
+                <label style="color:white; font-family:arial; text-decoration:none; padding:5px">Login:</label><br>
+                <input type="text" name="login"><br>
+                <label style="color:white; font-family:arial; text-decoration:none; padding:5px">Senha:</label><br>
+                <input type="password" name="senha"><br>
+
+                <label style="color:red; font-family:arial; text-decoration:none; padding:5px; font-size: 14px">
+
+                    <b>Atenção: Não foi possível validar seu acesso!
                         Caso você seja um usuario cadastrado verifique seu Login e Senha,
                         Caso contrário, entre em contato com o Administrador.
-                        </b>
-                    </label><br>
-                    <input type="submit" value="Entrar">
-                </form>
-            </div>
-        </center>
-    </body>
+                    </b>
+                </label><br>
+                <input type="submit" value="Entrar">
+            </form>
+        </div>
+    </center>
+</body>
 </html>
